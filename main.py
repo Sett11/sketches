1da8ccc7292a52b02a967c9371e1d8982382e587
Jhonny <gonzodzen@mail.ru>
2024-01-15 18:54:30 +0300

    3.7 (stepic)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-a,b=set(),set()
+d={'север':0,'юг':0,'восток':0,'запад':0}
 for i in range(int(input())):
-    a.add(input().lower())
-for i in range(int(input())):
-    b.update([j for j in input().lower().split() if j not in a])
-print(*list(b),sep='\n')
+    a,b=input().split()
+    d[a]+=int(b)
+print(d['восток']-d['запад'],d['север']-d['юг'])
