12622d8d3d14d0afff8aecadecb3a6c31ebc0c89
Jhonny <gonzodzen@mail.ru>
2024-01-15 23:01:02 +0300

    1.2 (stepik)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-r=0
-for i in range(int(input())):
-    r+=int(input())
+objects,s,r=[1, 2, 1, 2, 3],set(),0
+for i in objects:
+    n=id(i)
+    if n not in s:
+        s.add(n)
+        r+=1
 print(r)
