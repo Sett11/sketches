d5abad1fe873a883ae6795b3c95f65397329ef58
Jhonny <gonzodzen@mail.ru>
2024-01-17 00:00:59 +0300

    Buffer (stepik)

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-class MoneyBox:
-    def __init__(self,size):
-        self.size=size
-        self.count=0
+class Buffer:
+    def __init__(self):
+        self.a=[]
+    def add(self,*a):
+        self.a+=list(a)
+        while len(self.a)>=5:
+            s=sum(self.a[:5])
+            self.a=self.a[5:]
+            print(s)
+    def get_current_part(self):
+        return self.a
 
-    def can_add(self,v):
-        return self.count+v<=self.size
-
-    def add(self,v):
-        self.count+=v
+buf = Buffer()
+buf.add(1, 2, 3)
+print(buf.get_current_part()) # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+print(buf.get_current_part()) # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+print(buf.get_current_part()) # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+print(buf.get_current_part()) # вернуть [1]
