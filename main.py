df73a7bd177b740cacc819405c99714b6623c9f6
Jhonny <gonzodzen@mail.ru>
2024-01-16 22:55:37 +0300

    MoneyBox (stepik)

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-d={'global':{'par':None,'vars':[]}}
+class MoneyBox:
+    def __init__(self,size):
+        self.size=size
+        self.count=0
 
-def f(x,y):
-    if d[x]['par'] is None and y not in d[x]['vars']:
-        return None
-    if y in d[x]['vars']:
-        return x
-    return f(d[x]['par'],y)
+    def can_add(self,v):
+        return self.count+v<=self.size
 
-for _ in range(int(input())):
-    a,b,c=input().split()
-    if a=='add':
-        d[b]['vars'].append(c)
-    if a=='create':
-        d[b]={'par':c,'vars':[]}
-    if a=='get':
-        print(f(b,c))
+    def add(self,v):
+        self.count+=v
