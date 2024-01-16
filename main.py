f9b50fbe3bcd46b4af6686c564e5d3f3a9a03e76
Jhonny <gonzodzen@mail.ru>
2024-01-16 22:37:25 +0300

    1.4 Namespaces and scopes (stepik)

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import matp
-n,k=map(int,input().split())
+d={'global':{'par':None,'vars':[]}}
 
-def f(a,b):
-    if b==0:
-        return 1
-    if a<b:
-        return 0
-    return f(a-1,b)+f(a-1,b-1)
+def f(x,y):
+    if d[x]['par'] is None and y not in d[x]['vars']:
+        return None
+    if y in d[x]['vars']:
+        return x
+    return f(d[x]['par'],y)
 
-print(f(n,k))
+for _ in range(int(input())):
+    a,b,c=input().split()
+    if a=='add':
+        d[b]['vars'].append(c)
+    if a=='create':
+        d[b]={'par':c,'vars':[]}
+    if a=='get':
+        print(f(b,c))
