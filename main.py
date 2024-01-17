9308e2c7dd1c34b56d21e35f515a8228b383ccaf
Jhonny <gonzodzen@mail.ru>
2024-01-17 20:39:59 +0300

    1.6 Class inheritance (stepik)

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-class Buffer:
-    def __init__(self):
-        self.a=[]
-    def add(self,*a):
-        self.a+=list(a)
-        while len(self.a)>=5:
-            s=sum(self.a[:5])
-            self.a=self.a[5:]
-            print(s)
-    def get_current_part(self):
-        return self.a
+d={}
 
-buf = Buffer()
-buf.add(1, 2, 3)
-print(buf.get_current_part()) # вернуть [1, 2, 3]
-buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
-print(buf.get_current_part()) # вернуть [6]
-buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
-print(buf.get_current_part()) # вернуть []
-buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
-print(buf.get_current_part()) # вернуть [1]
+def f(a,b,q):
+    if a not in d or not d[a].get('children'):
+        q.append('No')
+        return q
+    if b in d[a]['children']:
+        q.append('Yes')
+        return q
+    for i in d[a].get('children',[]):
+        f(i,b,q)
+    return q
+
+for _ in range(int(input())):
+    s=input().split(' : ')
+    t=s.pop(0)
+    if s:
+        s=s[0].split()
+    if t not in d:
+        d[t]={'parents':[*s],'children':[]}
+    else:
+        d[t]['parents'].extend([*s])
+    if s:
+        for i in s:
+            if i not in d:
+                d[i]={'parents':[],'children':[t]}
+            else:
+                d[i]['children'].append(t)
+    else:
+        d[t]['parents'].append(t)
+
+for i in range(int(input())):
+    v=f(*input().split(),[])
+    if v is None:
+        print('No')
+    else:
+        print('No' if all(k=='No' for k in v) else 'Yes')
+
+# d={'Obj': {'parents': [], 'children': ['A', 'B', 'C']}, 'A': {'parents': ['Obj'], 'children': ['F', 'K']}, 'B': {'parents': ['Obj'], 'children': ['K', 'Z']}, 'C': {'parents': ['Obj'], 'children': ['G']}, 'F': {'parents': ['A'], 'children': ['L']}, 'K': {'parents': ['A', 'B'], 'children': ['L']}, 'Z': {'parents': ['B'], 'children': ['P']}, 'G': {'parents': ['C'], 'children': ['N']}, 'L': {'parents': ['F', 'K'], 'children': ['P']}, 'N': {'parents': ['G'], 'children': ['P']}, 'P': {'parents': ['L', 'Z', 'N'], 'children': []}}
+
+# print(f('Obj','B',[]))
