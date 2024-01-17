cbb8a86944d0d5417bb6bf1c9cb28e532ec188b6
Jhonny <gonzodzen@mail.ru>
2024-01-17 20:56:08 +0300

    1.6 ExtendedStack (stepik)

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-d={}
+class ExtendedStack(list):
+    def sum(self):
+        self.append(self.pop()+self.pop())
+    def sub(self):
+        self.append(self.pop()-self.pop())
+    def mul(self):
+        self.append(self.pop()*self.pop())
+    def div(self):
+        self.append(self.pop()//self.pop())
 
-def f(a,b,q):
-    if a not in d or not d[a].get('children'):
-        q.append('No')
-        return q
-    if b in d[a]['children']:
-        q.append('Yes')
-        return q
-    for i in d[a].get('children',[]):
-        f(i,b,q)
-    return q
-
-for _ in range(int(input())):
-    s=input().split(' : ')
-    t=s.pop(0)
-    if s:
-        s=s[0].split()
-    if t not in d:
-        d[t]={'parents':[*s],'children':[]}
-    else:
-        d[t]['parents'].extend([*s])
-    if s:
-        for i in s:
-            if i not in d:
-                d[i]={'parents':[],'children':[t]}
-            else:
-                d[i]['children'].append(t)
-    else:
-        d[t]['parents'].append(t)
-
-for i in range(int(input())):
-    v=f(*input().split(),[])
-    if v is None:
-        print('No')
-    else:
-        print('No' if all(k=='No' for k in v) else 'Yes')
-
-# d={'Obj': {'parents': [], 'children': ['A', 'B', 'C']}, 'A': {'parents': ['Obj'], 'children': ['F', 'K']}, 'B': {'parents': ['Obj'], 'children': ['K', 'Z']}, 'C': {'parents': ['Obj'], 'children': ['G']}, 'F': {'parents': ['A'], 'children': ['L']}, 'K': {'parents': ['A', 'B'], 'children': ['L']}, 'Z': {'parents': ['B'], 'children': ['P']}, 'G': {'parents': ['C'], 'children': ['N']}, 'L': {'parents': ['F', 'K'], 'children': ['P']}, 'N': {'parents': ['G'], 'children': ['P']}, 'P': {'parents': ['L', 'Z', 'N'], 'children': []}}
-
-# print(f('Obj','B',[]))
+a=ExtendedStack()
+a.append(1)
+a.extend([2,3,4])
+print(a)
+a.mul()
+print(a)
