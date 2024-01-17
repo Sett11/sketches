7518e9496cbd5a10ddf1b07c4c671f211b96b166
Jhonny <gonzodzen@mail.ru>
2024-01-17 22:09:59 +0300

    2.1 Errors and Exceptions (stepik)

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import time
+def foo():
+    pass
 
-class Loggable:
-    def log(self, msg):
-        print(str(time.ctime()) + ": " + str(msg))
-
-class LoggableList(list,Loggable):
-    def append(self,x):
-        self+=[x]
-        super(LoggableList,self).log(x)
-        
-l=LoggableList()
-l.append(1)
-l.append(7)
-print(l)
+try:
+    foo()
+except AssertionError:
+    print('AssertionError')
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ArithmeticError:
+    print('ArithmeticError')
