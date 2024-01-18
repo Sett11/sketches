646f503d6b8368fbb8f8087f45571274fc83d11a
Jhonny <gonzodzen@mail.ru>
2024-01-18 22:06:54 +0300

    2.3 Iterators and generators (stepik)

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-class multifilter:
-    def judge_half(pos,neg):
-        return pos>=neg
+def check_prime(n):
+    return n>=2 and all(n%i for i in range(2,int(n**.5+1)))
 
-    def judge_any(pos,neg):
-        return pos
+def primes():
+    n=2
+    while True:
+        if check_prime(n):
+            yield n
+        n+=1
 
-    def judge_all(pos,neg):
-        return not neg
-
-    def __init__(self, iterable, *funcs, judge=judge_any):
-        self.iterable=iterable
-        self.funcs=funcs
-        self.judge=judge
-
-    def __iter__(self):
-        r=[]
-        for i in self.iterable:
-            t=[j(i) for j in self.funcs]
-            if self.judge(t.count(True),t.count(False)):
-                r.append(i)
-        return (i for i in r)
-
-def mul2(x):
-    return x % 2 == 0
-
-def mul3(x):
-    return x % 3 == 0
-
-def mul5(x):
-    return x % 5 == 0
-
-a = [i for i in range(31)]
-
-print(list(multifilter(a, mul2, mul3, mul5,judge=multifilter.judge_all))) 
+p=primes()
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
