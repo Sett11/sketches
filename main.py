fd632a4ac1bc513167939daee04f6dccd658dfc3
Jhonny <gonzodzen@mail.ru>
2024-01-18 23:17:13 +0300

    2.4 Working with the file system and files (stepik)

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-with open('dataset_24465_4.txt') as r,open('b.txt','a') as a:
-    s=r.read().splitlines()
-    for i in s[::-1]:
-        a.write(i+'\n')
+import os
+
+r=[]
+
+for i,_,k in os.walk('.'):
+    if any(p.endswith('.py') for p in k):
+        r.append(i[2:])
+
+with open('solve.txt','w') as w:
+    for i in sorted(r):
+        w.write(i+'\n')
