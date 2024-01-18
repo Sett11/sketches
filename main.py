6209a2d04fbab91228d6046573283cbcef35efc0
Jhonny <gonzodzen@mail.ru>
2024-01-18 22:44:38 +0300

    2.4 Working with the file system and files (stepik)

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-def check_prime(n):
-    return n>=2 and all(n%i for i in range(2,int(n**.5+1)))
-
-def primes():
-    n=2
-    while True:
-        if check_prime(n):
-            yield n
-        n+=1
-
-p=primes()
-print(next(p))
-print(next(p))
-print(next(p))
-print(next(p))
-print(next(p))
-print(next(p))
-print(next(p))
-print(next(p))
+with open('dataset_24465_4.txt') as r,open('b.txt','a') as a:
+    s=r.read().splitlines()
+    for i in s[::-1]:
+        a.write(i+'\n')
