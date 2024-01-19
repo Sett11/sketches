7fbf755640acbbe31820bc4677b64b3fb27a3e19
Jhonny <gonzodzen@mail.ru>
2024-01-19 11:49:18 +0300

    3.2 Regular expressions in Python (stepik)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sys import stdin
-from re import sub,IGNORECASE
+from re import sub
+
+def f(s):
+    return f'{s[1]}{s[0]}{s[2:]}' if len(s)>1 else s
 
 for i in stdin:
-    j=i.strip()
-    print(sub(r'\b(a)+\b','argh',j,count=1,flags=IGNORECASE))
+    print(sub(r'\b(\w)+\b',lambda x:f(x.group()),i.strip()))
