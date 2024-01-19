3a7a33f523e419ada2712aaeafcb36715df463bc
Jhonny <gonzodzen@mail.ru>
2024-01-19 12:29:09 +0300

    3.2 Regular expressions in Python (stepik)

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from sys import stdin
-from re import sub
+from re import findall
+
+def f(a):
+    state='a'
+    for i in range(len(a)):
+        if not a[i].isdigit():
+            return False
+        if state=='a':
+            if a[i]=='1':
+                state='b'
+        elif state=='b':
+            if a[i]=='1':
+                state='a'
+            else:
+                state='c'
+        else:
+            if a[i]=='0':
+                state='b'
+    return state=='a'
 
 for i in stdin:
-    print(sub(r'(\w)\1+',lambda x:x.group()[0],i.strip()))
+    j=i.strip()
+    if f(findall(r'.',j)):
+        print(j)
