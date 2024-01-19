c794db51dc37badd3ae4760cb799ba32b8e823fb
Jhonny <gonzodzen@mail.ru>
2024-01-19 10:47:54 +0300

    3.2 Regular expressions in Python (stepik)

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-a, b = input(), input()
+from sys import stdin
+from re import findall
 
-
-def f(a, b):
-    if b not in a:
-        return 0
-    r, i = 0, a.find(b)
-    while i != -1:
-        i = a.find(b, i + 1)
-        r += 1
-    return r
-
-
-print(f(a, b))
+for i in stdin:
+    j=i.strip()
+    if len(findall(r'cat',j))>1:
+        print(j)
