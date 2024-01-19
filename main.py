32421716dca79518b5b1287703de4b0414408db1
Jhonny <gonzodzen@mail.ru>
2024-01-19 11:22:33 +0300

    3.2 Regular expressions in Python (stepik)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sys import stdin
-from re import sub
+from re import sub,IGNORECASE
 
 for i in stdin:
     j=i.strip()
-    print(sub(r'human','computer',j))
+    print(sub(r'\b(a)+\b','argh',j,count=1,flags=IGNORECASE))
