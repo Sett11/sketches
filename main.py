02f63b126768dbbd4cc2f965e420c3b4c97a3cb2
Jhonny <gonzodzen@mail.ru>
2024-01-19 11:17:05 +0300

    3.2 Regular expressions in Python (stepik)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from sys import stdin
-from re import search
+from re import sub
 
 for i in stdin:
     j=i.strip()
-    if search(r'\b(\w+)\1\b',j):
-        print(j)
+    print(sub(r'human','computer',j))
