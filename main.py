3fe17f30bb8698ab78333ccae35ad98a9213a318
Jhonny <gonzodzen@mail.ru>
2024-01-20 19:31:35 +0300

    3.6 XML, ElementTree library, lxml library (stepik)

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from requests import get
-from json import loads
+from xml.etree.ElementTree import XMLParser
 
-header={"X-Xapp-Token":"eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZjhlNDBhNC0yNjMxLTQwNGEtYTJjYi0xYmQ1MjQ4MDU0NTciLCJleHAiOjE3MDYzNTk3ODQsImlhdCI6MTcwNTc1NDk4NCwiYXVkIjoiNWY4ZTQwYTQtMjYzMS00MDRhLWEyY2ItMWJkNTI0ODA1NDU3IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjY1YWJjMTY4YTVlNmU5MDAwYjYwODJiYyJ9.apICD_NbaGqssVKZg23R5bb0HvItZEhQ-SrCbu7i5WM"}
-
-q,res=[],[]
-f=open('dataset_24476_4.txt','r',encoding='utf-8')
-q=f.read().strip().split('\n')
-f.close()
-
-for i in q:
-    r=get("https://api.artsy.net/api/artists/"+i,headers=header)
-    l=loads(r.text)
-    res.append((l['sortable_name'],l['birthday']))
-
-res.sort(key=lambda x:(x[1],-ord(x[0][0])))
-
-with open('solve.txt','w',encoding='utf-8') as s:
-    for i in res:
-        s.write(i[0]+'\n')
+class C:
+    o={'d':0,'red':0,'green':0,'blue':0}
+    def start(self,tag,attr):
+        self.o['d']+=1
+        self.o[attr['color']]+=self.o['d']
+    def end(self,tag):
+        self.o['d']-=1
+    def data(self,data):
+        pass
+    def close(self):
+        return f"{self.o['red']} {self.o['green']} {self.o['blue']}"
+t=C()
+p=XMLParser(target=t)
+p.feed(input())
+print(p.close())
