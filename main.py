b95b808b8341a499c2d4d217c3dd06c7989f6f5a
Jhonny <gonzodzen@mail.ru>
2024-01-22 00:15:56 +0300

    Big number (stepik)

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-a,b=int(input()),int(input())
-print(f"Квадрат суммы {a} и {b} равен {(a+b)**2}",f"Сумма квадратов {a} и {b} равна {a**2+b**2}",sep='\n')
+a,b,c,d=int(input()),int(input()),int(input()),int(input())
+print(a**b+c**d)
