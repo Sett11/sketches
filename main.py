4e936baf44ca8cc8e72e95d451b3fe51354e046b
Jhonny <gonzodzen@mail.ru>
2024-01-22 01:10:15 +0300

    4.1 Age group (stepik)

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print(min(int(input()),int(input()),int(input()),int(input())))
+n=int(input())
+a=['детство','молодость', 'зрелость','старость']
+print(a[0] if n<15 else a[1] if n<25 else a[2] if n<60 else a[3])
