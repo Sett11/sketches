9693a0a8680f5a104e665cb549027c181338ff7f
Jhonny <gonzodzen@mail.ru>
2024-01-21 22:11:43 +0300

    2.4 "Generation Python": a course for beginners (stepik)

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from xml.etree.ElementTree import XMLParser
-
-class C:
-    o={'d':0,'red':0,'green':0,'blue':0}
-    def start(self,tag,attr):
-        self.o['d']+=1
-        self.o[attr['color']]+=self.o['d']
-    def end(self,tag):
-        self.o['d']-=1
-    def data(self,data):
-        pass
-    def close(self):
-        return f"{self.o['red']} {self.o['green']} {self.o['blue']}"
-t=C()
-p=XMLParser(target=t)
-p.feed(input())
-print(p.close())
+f=lambda a,b:3*(a+b)**3+275*b**2-127*a-41
+print(f(int(input()),int(input())))
