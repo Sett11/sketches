ea978f48d29e875fe102ebc1d5170af7358b7fcf
Jhonny <gonzodzen@mail.ru>
2024-01-23 01:15:45 +0300

    7.3 Number sequence 3 🌶️

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 a,b=int(input()),int(input())
-r=[i for i in range(min(a,b),max(a,b)+1)]
-print(*(r[::-1] if a>b else r),sep='\n')
+print(*[i for i in range(a-(0 if a&1 else 1),b-(1 if b&1 else 0),-2)],sep='\n')
