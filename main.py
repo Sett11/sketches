776a476b12cf1abbdf40f232cd10ee5daff4e6f9
Jhonny <gonzodzen@mail.ru>
2024-01-23 01:22:20 +0300

    7.3 Multiplication table

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-a,b=int(input()),int(input())
-print(*[i for i in range(a,b+1) if i%17==0 or (i%3==0 and i%5==0) or str(i)[-1]=='9'],sep='\n')
+n=int(input())
+print(*[f'{n} x {i} = {n*i}' for i in range(1,11)],sep='\n')
