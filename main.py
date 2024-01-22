9d7e2c2f7edcadc74ce69a201682358ce622e979
Jhonny <gonzodzen@mail.ru>
2024-01-22 13:18:58 +0300

    4.3 Intersection of segments 🌶️🌶️

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-n=int(input())
-a,b='красный','черный'
-if n>0 and (n<11 or 18<n<29):
-    print(a if n&1 else b)
-elif n<37 and (11<=n<19 or n>28):
-    print(b if n&1 else a)
-elif not n:
-    print('зеленый')
-else:
-    print('ошибка ввода')
+a,b=sorted([[int(input()),int(input())],[int(input()),int(input())]])
+print(*set([max(a[0],b[0]),min(a[1],b[1])])) if min(b)<=max(a) else print('пустое множество')
