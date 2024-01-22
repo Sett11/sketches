a823651e423dbae2fd5555eaac3d9fbdcbd1adf0
Jhonny <gonzodzen@mail.ru>
2024-01-22 18:05:32 +0300

    6.3 Quadratic Equation 🌶️🌶️

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from math import ceil,floor
-
-n=float(input())
-print(floor(n)+ceil(n))
+a,b,c=float(input()),float(input()),float(input())
+d=b**2-4*a*c
+q=d**.5
+if d==0:
+    print(-b/(2*a))
+elif d>0:
+    print(*sorted([(-b-q)/(2*a),(-b+q)/(2*a)]),sep='\n')
+else:
+    print('Нет корней')
