16a163c73358968c1cc5b32a68e28810d32afe14
Jhonny <gonzodzen@mail.ru>
2024-01-22 14:48:51 +0300

    6.1 Largest and smallest

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('0.'+input().split('.')[1])
+from bisect import insort
+
+r=[]
+for i in range(5):
+    insort(r,int(input()))
+print(f"Наименьшее число = {r[0]}",f"Наибольшее число = {r[-1]}",sep='\n')
