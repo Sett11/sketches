cf954de211884aad1784a4719578c81959f94cf2
Jhonny <gonzodzen@mail.ru>
2024-01-22 13:26:46 +0300

    Beginning of the century

--- main.py
+++ main.py
@@ -1,2 +1,1 @@
-a,b=sorted([[int(input()),int(input())],[int(input()),int(input())]])
-print(*set([max(a[0],b[0]),min(a[1],b[1])])) if min(b)<=max(a) else print('пустое множество')
+print(['NO','YES'][input()[-2:]=='00'])
