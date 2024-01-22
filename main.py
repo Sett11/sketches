0303ed3baaa1f0fc1b07b545dbe0dbcaa95a2a98
Jhonny <gonzodzen@mail.ru>
2024-01-22 14:02:53 +0300

    Knight's move

--- main.py
+++ main.py
@@ -1,24 +1,2 @@
-a=[[(i,j) for j in range (1,9)] for i in range(1,9)]
-b,c,d,e=int(input())-1,int(input())-1,int(input()),int(input())
-q=set()
-i,j=b,c
-while i>=0 and j>=0:
-    q.add(a[i][j])
-    i-=1
-    j-=1
-i,j=b,c
-while i<8 and j<8:
-    q.add(a[i][j])
-    i+=1
-    j+=1
-i,j=b,c
-while i>=0 and j<8:
-    q.add(a[i][j])
-    i-=1
-    j+=1
-i,j=b,c
-while i<8 and j>=0:
-    q.add(a[i][j])
-    i+=1
-    j-=1
-print(['NO','YES'][(d,e) in q])
+b,c,d,e=int(input()),int(input()),int(input()),int(input())
+print(['NO','YES'][(d,e) in [(b-1,c+2),(b+1,c+2),(b-1,c-2),(b+1,c-2),(b+2,c-1),(b+2,c+1),(b-2,c-1),(b-2,c+1)]])
