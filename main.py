53067167baf5fbf85d90baff71f4d429abfc40fa
Jhonny <gonzodzen@mail.ru>
2024-01-22 16:26:18 +0300

    6.3 Trigonometric expression

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-a,b=float(input()),float(input())
-print((a+b)/2,(a*b)**.5,2*a*b/(a+b),(((a**2)+(b**2))/2)**.5,sep='\n')
+from math import sin,cos,tan,pi,radians
+
+n=radians(float(input()))
+print(sin(n)+cos(n)+tan(n)**2)
