1b7cf92ffa9c197f0f5cd095d15ae801e57af3ea
Jhonny <gonzodzen@mail.ru>
2024-01-22 10:48:57 +0300

    4.2 King's move 🌶️

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 a,b,c,d=int(input()),int(input()),int(input()),int(input())
-print(['NO','YES'][a==c or b==d])
+r=[]
+for i in range(a-1,a+2):
+    for j in range(b-1,b+2):
+        if i>=0 and i<9 and j>=0 and j<9:
+            r.append((i,j))
+print(['NO','YES'][(c,d) in r])
