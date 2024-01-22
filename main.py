ebacc50b1b722ff959d575cca6bcadcf2c790f02
Jhonny <gonzodzen@mail.ru>
2024-01-22 13:36:52 +0300

    Girls only

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-a,k=[[0]*8 for i in range(1,9)],True
-for i in range(8):
-    for j in range((0 if k else 1),8,2):
-        a[i][j]=1
-    k=not k
-b,c,d,e=int(input()),int(input()),int(input()),int(input())
-print(['NO','YES'][a[b-1][c-1]==a[d-1][e-1]])
+print(['NO','YES'][9<int(input())<16 and input()=='f'])
