2477038ea00449162e4b6403e4c366d9c334c5df
Jhonny <gonzodzen@mail.ru>
2024-01-23 01:20:23 +0300

    7.3 Number sequence 4

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 a,b=int(input()),int(input())
-print(*[i for i in range(a-(0 if a&1 else 1),b-(1 if b&1 else 0),-2)],sep='\n')
+print(*[i for i in range(a,b+1) if i%17==0 or (i%3==0 and i%5==0) or str(i)[-1]=='9'],sep='\n')
