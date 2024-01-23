e7c5729b779d6216a16c8ee1fa85fe7a1a0926b6
Jhonny <gonzodzen@mail.ru>
2024-01-24 00:52:02 +0300

    Flatten)

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('\n\n'.join(['\n'.join([f"{i} + {j} = {i+j}" for j in range(1,10)]) for i in range(1,int(input())+1)]))
+new_list=[]
+lst = [1, [2, 3], [[2], 5], 6]
+
+def flatten(lst) :
+    if all(isinstance(i,int) for i in lst):
+        [new_list.append(i) for i in lst]
+        return new_list
+    return flatten(sum([[i] if isinstance(i,int) else i for i in lst],[]))
+
+print(flatten(lst))
+print(new_list)
