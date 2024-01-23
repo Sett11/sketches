ea0f22859e53a847771c9610f02114b024c6d06c
Jhonny <gonzodzen@mail.ru>
2024-01-24 00:56:30 +0300

    7.8 Star Triangle 🌶️🌶️

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-new_list=[]
-lst = [1, [2, 3], [[2], 5], 6]
-
-def flatten(lst) :
-    if all(isinstance(i,int) for i in lst):
-        [new_list.append(i) for i in lst]
-        return new_list
-    return flatten(sum([[i] if isinstance(i,int) else i for i in lst],[]))
-
-print(flatten(lst))
-print(new_list)
+a=['*'*i for i in range(1,int(input())//2+2)]
+print(*(a+a[::-1][1:]),sep='\n')
