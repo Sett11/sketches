38040a4ec00190ec0bc067eefd350cfd985f4f5d
Jhonny <gonzodzen@mail.ru>
2024-01-23 22:56:51 +0300

    Code review-3

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-mx = -10e7
 s = 0
-for i in range(10):
-    x = int(input())
-    if x < 0:
-        s += x
-        if x > mx:
-            mx = max(x,mx)
-if s:
-    print(s,mx,sep='\n')
-else:
-    print('NO')
+for _ in range(7):
+    n = int(input())
+    if n % 2 == 0:
+        s+=n
+print(s)
