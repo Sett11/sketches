6396d6b7e76cf6a7cf07d66d2a94673f1101f965
Jhonny <gonzodzen@mail.ru>
2024-01-24 01:08:19 +0300

    7.8 12 months

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(*[str(i)*i for i in range(1,int(input())+1)],sep='\n')
+r=[]
+x=10
+for i in range(1,x):
+    for j in range(1,x):
+        for k in range(1,x):
+            if 28*i+30*j+31*k==365:
+                r.append((i,j,k))
+
+print(r)
