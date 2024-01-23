b3f05ffc31aa99de346401240b1423cca6734447
Jhonny <gonzodzen@mail.ru>
2024-01-23 12:34:39 +0300

    7.5 Second digit

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-from functools import reduce as r
-from operator import mul
-from statistics import mean
-
-l=list(map(int,input()))
-print(sum(l),len(l),r(mul,l),mean(l),l[0],l[0]+l[-1],sep='\n')
+print(input()[1])
