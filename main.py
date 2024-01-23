694c77434273fdd3bc3e068b070e448f48fcfc15
Jhonny <gonzodzen@mail.ru>
2024-01-23 09:31:47 +0300

    7.3 Sum of numbers 2

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from math import log
-
-total,n=1,int(input())
-
-for i in range(2,n+1):
-    total+=1/i
-
-print(total-log(n))
+print(sum(i for i in range(1,int(input())+1) if str(i**2)[-1] in '258'))
