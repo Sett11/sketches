1f755c694011f3e68415af1a3b2b1183de1fc9c3
Jhonny <gonzodzen@mail.ru>
2024-01-24 21:54:47 +0300

    7.9 Numerical triangle 3

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-n,r=int(input()),[[str(1)]]
-
-while len(r)!=n:
-    x,t=int(r[-1][-1])+1,[]
-    for i in range(x,x+len(r[-1])+1):
-        t.append(str(i))
-    r.append(t)
-
-print(*[' '.join(i) for i in r],sep='\n')
+n=int(input())
+print(*list(zip(*[[''.join([str(k) for k in j[:k]+j[:k][::-1][1:]]) for j in sum([[[i for i in range(1,n+1)]]]*n,[])] for k in range(1,n+1)]))[0],sep='\n')
