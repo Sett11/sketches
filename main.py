9eb20c7b8acaf349e89e5f383ed8159c1640906d
Jhonny <gonzodzen@mail.ru>
2024-01-24 21:32:16 +0300

    7.9 Numerical triangle 2

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-for i in range(1,151):
-    for j in range(i,151):
-        for k in range(j,151):
-            for h in range(k,151):
-                for n in range(h,151):
-                    x=int(i**5)+int(j**5)+int(k**5)+int(h**5)
-                    if x==int(n**5):
-                        print(i+j+k+h+n)
-                        break
-                    if n**5>x:
-                        break
+n,r=int(input()),[[str(1)]]
 
+while len(r)!=n:
+    x,t=int(r[-1][-1])+1,[]
+    for i in range(x,x+len(r[-1])+1):
+        t.append(str(i))
+    r.append(t)
+
+print(*[' '.join(i) for i in r],sep='\n')
