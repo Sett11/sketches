bea21417386ca30bc8269362050c95dc2df651e7
Jhonny <gonzodzen@mail.ru>
2024-01-24 23:16:35 +0300

    star frame

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-n = 4
-count = 0
-maximum = -10e9
-for i in range(n):
-    x = int(input())
-    if x&1:
-        count += 1
-        if x > maximum:
-            maximum = x
-if count > 0:
-    print(count)
-    print(maximum)
-else:
-    print('NO')
+n,s=int(input()),['*'*19]
+print(*(s+['*'+' '*17+'*' for _ in range(n-2)]+s),sep='\n')
