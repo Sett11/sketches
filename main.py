725090640f964a5245a3f6c3cbfd606413b07689
Jhonny <gonzodzen@mail.ru>
2024-01-25 00:03:39 +0300

    Ramanujan numbers 🌶️

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from functools import reduce as r
-from operator import mul
 from collections import Counter
 
-s=input()
-c=Counter(s)
-a=list(map(int,s))
-b=list(filter(lambda x:x>7,a))
-print(c['3'],c[s[-1]],sum(map(lambda e:e[1],(filter(lambda x:int(x[0])%2==0,c.items())))),sum(filter(lambda x:x>5,a)),r(mul,b) if len(b)>1 else 1 if not b else b[0],c.get('0',0)+c.get('5',0),sep='\n')
+s,r=set(),[]
+
+for i in range(1,33):
+    for j in range(1,33):
+        if i!=j:
+            r.append(i**3+j**3)
+
+print(sorted(list(map(lambda e:e[0],filter(lambda x:x[1]==4,Counter(r).items())))))
