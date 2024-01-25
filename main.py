aea01eaa3439655c5e76427ec1cfad5c11372741
Jhonny <gonzodzen@mail.ru>
2024-01-25 20:44:53 +0300

    9ю4 A minute of genetics

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print(len(input().split()))
+from collections import Counter
+
+c=Counter(input().lower())
+print(f"Аденин: {c['а']}",f"Гуанин: {c['г']}",f"Цитозин: {c['ц']}",f"Тимин: {c['т']}",sep='\n')
