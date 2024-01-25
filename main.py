25452c94c8d27c729d9aca6f1dafe3295bbb993d
Jhonny <gonzodzen@mail.ru>
2024-01-26 00:05:09 +0300

    11.4 Function value

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print(list(''.join([input() for _ in range(int(input()))])))
+f=lambda x:x**2+2*x+1
+a=[int(input()) for _ in range(int(input()))]
+print(*a,sep='\n')
+print()
+print(*map(f,a),sep='\n')
