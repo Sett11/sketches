501ff52ba3e17d4197bb3f0a67a51eb9c1f4a398
Jhonny <gonzodzen@mail.ru>
2024-01-26 00:31:52 +0300

    11.5 Line by line output

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-a,b,c=[],[],[]
-for i in range(int(input())):
-    n=int(input())
-    a.append(n) if n<0 else c.append(n) if n>0 else b.append(n)
-print(*(a+b+c),sep='\n')
+print(*input().split(),sep='\n')
