5541e9c6f3676ea7101d5928ca41f3e1d92c8497
Jhonny <gonzodzen@mail.ru>
2024-01-25 18:57:02 +0300

    9.1 In column 1

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from collections import Counter
-
-s,r=set(),[]
-
-for i in range(1,33):
-    for j in range(1,33):
-        if i!=j:
-            r.append(i**3+j**3)
-
-print(sorted(list(map(lambda e:e[0],filter(lambda x:x[1]==4,Counter(r).items())))))
+print(*input()[0:100:2],sep='\n')
