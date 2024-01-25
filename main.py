384b17fd189cca9dece28e5261ce2e1836f19657
Jhonny <gonzodzen@mail.ru>
2024-01-26 00:24:42 +0300

    11.4 Google search - 2 🌶️🌶️

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-a=[input() for _ in range(int(input()))]
-c=input().lower()
-print(*filter(lambda x:c in x.lower(),a),sep='\n')
+a,b=[input() for _ in range(int(input()))],[input() for _ in range(int(input()))]
+print(*filter(lambda x:all(i.lower() in x.lower() for i in b),a),sep='\n')
