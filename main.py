644ac7625d92259b8c21d0dfa1db0325b8f08275
Jhonny <gonzodzen@mail.ru>
2024-01-25 23:49:58 +0300

    11.3 Characters of all lines

--- main.py
+++ main.py
@@ -1,2 +1,1 @@
-from itertools import zip_longest
-print(*list(zip_longest(*[input() for _ in range(int(input()))],fillvalue=''))[int(input())-1],sep='')
+print(list(''.join([input() for _ in range(int(input()))])))
