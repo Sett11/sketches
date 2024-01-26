c50e8be3a8950613ebdd354890f7ad940c558b28
Jhonny <gonzodzen@mail.ru>
2024-01-26 12:17:28 +0300

    Rearrange min and max

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-a,c=input().split(),0
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        if a[i]==a[j]:
-            c+=1
-print(c)
+a=list(map(int,input().split()))
+i_min,i_max=a.index(min(a)),a.index(max(a))
+a[i_min],a[i_max]=a[i_max],a[i_min]
+print(*a)
