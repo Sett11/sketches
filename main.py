c9e54d7d28cddc3fdac9a39219a64a72c0a421e3
Jhonny <gonzodzen@mail.ru>
2024-01-26 15:45:19 +0300

    13.2 Star triangle

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-def draw_box():
-    return """*
-**
-***
-****
-*****
-******
-*******
-********
-*********
-**********"""
+def draw_triangle(q,k):
+    n=k//2+(1 if k&1 else 0)
+    a=[q*i for i in range(1,n+1)]
+    a+=a[::-1][1:]
+    return '\n'.join(a)
 
-print(draw_box())
+fill = input()
+base = int(input())
+
+print(draw_triangle(fill, base))
