f0e93535947b9389d5d444373e7a0e5d73d4b29b
Jhonny <gonzodzen@mail.ru>
2024-01-27 17:57:41 +0300

    Binomial coefficient 🌶️

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-a,b,c=int(input()),int(input()),int(input())
-d=b**2-4*a*c
-q=d**.5
-if d==0:
-    n=-b/(2*a)
-    print(n,n)
-else:
-    print(*sorted([(-b-q)/(2*a),(-b+q)/(2*a)]))
+from math import factorial as fack
+
+def f(n,k):
+    return fack(n)//(fack(k)*fack((n-k)))
+
+n = int(input())
+k = int(input())
+
+print(f(n,k))
