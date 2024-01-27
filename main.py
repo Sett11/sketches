f30d1509d2532d8bfb667cc3f427b45af533581f
Jhonny <gonzodzen@mail.ru>
2024-01-27 16:41:49 +0300

    13.5 Next Prime 🌶️🌶️

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-f=lambda n:n>1 and all(n%i for i in range(2,int(n**.5)+1))
-print(f(int(input())))
+f1=lambda n:n>1 and all(n%i for i in range(2,int(n**.5)+1))
+f2=lambda n:next(i for i in range(n+1,n+100) if f1(i))
+print(f2(int(input())))
