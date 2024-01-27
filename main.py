f9e9f9e7e97c76fad0f144d9ba1a8f52d4f8c054
Jhonny <gonzodzen@mail.ru>
2024-01-27 17:06:22 +0300

    13.5 Correct bracket sequence 🌶️

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-f=lambda n:n>1 and all(n%i for i in range(2,int(n**.5)+1))
-try:
-    a,b,c=input().split(':')
-    print(a==a[::-1] and f(int(b)) and int(c)%2==0)
-except:
-    print(False)
+from collections import deque
+
+def f(s):
+    q=deque()
+    for i in s:
+        if i=='(':
+            q.append(i)
+        else:
+            if not q or q.pop()+i!='()':
+                return 1==2
+    return not q
+
+print(f(input()))
