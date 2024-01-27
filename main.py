189d5c2a0323c081435c3b2b3f59d6880fd3afbe
Jhonny <gonzodzen@mail.ru>
2024-01-27 17:40:32 +0300

    13.6 Roots of the equation 🌶️🌶️

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from math import pi
-
-def f(r):
-    return 2*pi*r,pi*r**2
-
-print(*f(float(input())))
+a,b,c=int(input()),int(input()),int(input())
+d=b**2-4*a*c
+q=d**.5
+if d==0:
+    n=-b/(2*a)
+    print(n,n)
+else:
+    print(*sorted([(-b-q)/(2*a),(-b+q)/(2*a)]))
