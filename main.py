19020ca4e04d623f606203e2d45aa51b8a3bfb07
Jhonny <gonzodzen@mail.ru>
2024-01-27 16:20:25 +0300

    13.4 Merge lists 2

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-def f(a,b):
-    return sorted(a+b)
-
-a = [int(c) for c in input().split()]
-b = [int(c) for c in input().split()]
-
-print(f(a, b))
+print(*sorted(map(int,sum([input().split() for _ in range(int(input()))],[]))))
