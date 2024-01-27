891546824652a56b3a29beed4630986646a59de5
Jhonny <gonzodzen@mail.ru>
2024-01-27 16:51:28 +0300

    13.5 Exactly in one

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def f(s):
-    a=[False]*3+[len(s)>=8]
-    for i in s:
-        if i.islower():
-            a[0]=1==1
-        if i.isupper():
-            a[1]=1==1
-        if i.isdigit():
-            a[2]=1==1
-    return all(i for i in a)
+f=lambda a,b:len(a)==len(b) and len([i for i,j in enumerate(a) if a[i]!=b[i]])==1
 
-print(f(input()))
+print(f(input(),input()))
