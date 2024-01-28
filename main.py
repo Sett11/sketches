900093fc285fd06f6b0c668c6940152b978615c1
Jhonny <gonzodzen@mail.ru>
2024-01-28 12:57:42 +0300

    Convert base

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from string import ascii_lowercase as a,ascii_uppercase as b
-from re import sub
-
-s=input().split()
-f=lambda s,l: ''.join([a[(a.index(i)+l)%26] if i in a else b[(b.index(i)+l)%26] if i in b else i for i in s])
-
-print(' '.join(f(i,len(sub(r'[^A-z]','',i))) for i in s))
+def number_system():
+    return [a for a in range(10) if 8 * a ** 1 + 8 * a **0  == ((3 * a ** 1) + (2 * a ** 0)) + ((2 * a ** 1) + (2 * a ** 0)) + ((1 * a ** 1) + (6 * a ** 0)) + ((1 * a ** 1) + (7 * a ** 0))]
+                                                                                                                                       
+print(number_system())
