91db16659e869d69c359c3a752fab0249864e3da
Jhonny <gonzodzen@mail.ru>
2024-01-29 22:27:50 +0300

    2.2 Coordinate quarters

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-def josef(n,k):
-    k-=1
-    a,i=list(range(1,n+1)),k
-    while len(a)>1:
-        a.pop(i)
-        i=(i+k)%len(a)
-    return a[0]
+d={"Первая четверть": 0,
+"Вторая четверть": 0,
+"Третья четверть": 0,
+"Четвертая четверть": 0}
 
-print(josef(int(input()),int(input())))
+for x,y in [list(map(int,input().split())) for i in range(int(input()))]:
+    if x>0 and y>0:
+        d["Первая четверть"]+=1
+    elif x<0 and y>0:
+        d["Вторая четверть"]+=1
+    elif x<0 and y<0:
+        d["Третья четверть"]+=1
+    elif x>0 and y<0:
+        d["Четвертая четверть"]+=1
+
+for i in d:
+    print(f'{i}: {d[i]}')
