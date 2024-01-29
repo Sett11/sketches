4aa0ada027bea5366b7f5f98e01c571c986c2b5e
Jhonny <gonzodzen@mail.ru>
2024-01-29 08:28:32 +0300

    Line cost

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-a,b=float(input()),float(input())
-c=a/(b*b)
-print('Оптимальная масса' if 18.5<=c<=25 else 'Недостаточная масса' if c<18.5 else 'Избыточная масса')
+a,b=divmod(len(input())*60,100)
+print(f'{a} р. {b} коп.')
