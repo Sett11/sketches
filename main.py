298f74485ff2e1f4461b30068583384cfb0b10b3
Jhonny <gonzodzen@mail.ru>
2024-01-29 22:37:14 +0300

    2.2 More than previous

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-d={"Первая четверть": 0,
-"Вторая четверть": 0,
-"Третья четверть": 0,
-"Четвертая четверть": 0}
-
-for x,y in [list(map(int,input().split())) for i in range(int(input()))]:
-    if x>0 and y>0:
-        d["Первая четверть"]+=1
-    elif x<0 and y>0:
-        d["Вторая четверть"]+=1
-    elif x<0 and y<0:
-        d["Третья четверть"]+=1
-    elif x>0 and y<0:
-        d["Четвертая четверть"]+=1
-
-for i in d:
-    print(f'{i}: {d[i]}')
+a=list(map(int,input().split()))
+print(len([i for i in range(1,len(a)) if a[i]>a[i-1]]))
