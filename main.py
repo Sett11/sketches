bba98e92fa3dd12f5001e54e354848ac70e1aa4d
Jhonny <gonzodzen@mail.ru>
2024-01-29 22:59:45 +0300

    2.2 Product of numbers

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(len(set(input().split())))
+a,n,v=[int(input()) for _ in range(int(input()))],int(input()),False
+for i in range(len(a)):
+    if v:
+        break
+    for j in range(i+1,len(a)):
+        if a[i]*a[j]==n:
+            print('ДА')
+            v=True
+            break
+else:
+    print('НЕТ')
