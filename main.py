b3e54373888f1ebb7f2a68865fe2a8d32c801c53
Jhonny <gonzodzen@mail.ru>
2024-01-29 08:25:21 +0300

    Body mass index

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-a,b=int(input()),int(input())
-print(a+b,a-b,a*b,a/b,*divmod(a,b),(a**10+b**10)**.5,sep='\n')
+a,b=float(input()),float(input())
+c=a/(b*b)
+print('Оптимальная масса' if 18.5<=c<=25 else 'Недостаточная масса' if c<18.5 else 'Избыточная масса')
