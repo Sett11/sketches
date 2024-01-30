98be439166ac5d7064d4be45cd8f8a342adc8c14
Jhonny <gonzodzen@mail.ru>
2024-01-30 10:08:52 +0300

    2.2 Silicon Valley 🌶️🌶️

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from re import sub
-
-s=input()
-print(max(map(int,sub(r'(.)\1*',lambda x:' '+str(len(x.group())) if x.group()[0]=='Р' else '',s).split()),default=0))
+def f(s,k='anton'):
+    n,r=0,[]
+    for i in range(5):
+        n=s.find(k[i],n)
+        r.append(n)
+    return all(i!=-1 for i in r)
+print(*[j+1 for j,k in enumerate([input() for _ in range(int(input()))]) if f(k)])
