04aa589f5a4c27ff0d01395fa50e0f8638dc0a35
Jhonny <gonzodzen@mail.ru>
2024-01-31 22:23:41 +0300

    Maximum in the table

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 n,m=int(input()),int(input())
-r=[['0']*m for _ in range(n)]
-r[0]=' '.join(r[0]).ljust(m*3)
-for i in range(1,n):
-    for j in range(1,m):
-        r[i][j]=str(i*j)
-    r[i]=' '.join(r[i]).ljust(m*3)
-print(*r,sep='\n')
+r=[list(map(int,input().split())) for _ in range(n)]
+k,v=max(sum(r,[])),False
+for i in range(n):
+    if v:
+        break
+    for j in range(m):
+        if r[i][j]==k:
+            print(i,j)
+            v=True
+            break
