8fc2de51c01b110f8ccf4060497424b3679d899e
Jhonny <gonzodzen@mail.ru>
2024-01-31 08:19:18 +0300

    Pascal's triangle 1 🌶️

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n=int(input())
-a=[i for i in range(1,n+1)]
-for i in range(1,n+1):
-    print(a[:i])
+a,n=[[1],[1,1],[1,2,1]],int(input())
+for i in range(n-2):
+    k,t=a[-1],[1]
+    for j in range(1,len(k)):
+        t.append(k[j]+k[j-1])
+    a.append(t+[1])
+print(a[n])
