0cdfa6be8769f6be0c372c8dc560193ae7b085a1
Jhonny <gonzodzen@mail.ru>
2024-01-31 21:17:20 +0300

    4.4 Maximum in area 2 🌶️

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-a,m=[list(map(int,input().split())) for _ in range(int(input()))],float('-inf')
-for i in range(len(a)):
-    for j in range(i+1):
-        m=max(a[i][j],m)
-print(m)
+a,m=[[-3, 1, 4, -3],
+     [-9, -3, -3, -10],
+     [-4, -3, -3, -2],
+     [-3, 0, 0, -3]],-100#[list(map(int,input().split())) for _ in range(int(input()))],-100
+n,k=len(a),1
+r,q=[],[]
+for i in range(n):
+    t,w=[],[]
+    for j in range(k):
+        #m=max(a[i][j],a[i][::-1][j],m)
+        t.append(a[i][j])
+        w.append(a[i][::-1][j])
+    k=1 if i==n-2 else k+1 if i<n//2 and i!=n-1 else k-1
+    r.append(t)
+    q.append(w)
+print(r,q)
