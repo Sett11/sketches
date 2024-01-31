56d171d19e2b6bd33f203ee374b19782782072bf
Jhonny <gonzodzen@mail.ru>
2024-01-31 08:35:09 +0300

    4.3 Packing duplicates 🌶️

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-a,n=[[1],[1,1],[1,2,1]],int(input())
-for i in range(n-2):
-    k,t=a[-1],[1]
-    for j in range(1,len(k)):
-        t.append(k[j]+k[j-1])
-    a.append(t+[1])
-for i in range(n):
-    print(*a[i])
+from re import sub
+
+print([list(i) for i in sub(r'(.)\1*',lambda x:' '+x.group()+' ',''.join(input().split())).split()])
