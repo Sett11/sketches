4c26ed0efbd565fa40082fa78fb3a8cddbb8af14
Jhonny <gonzodzen@mail.ru>
2024-01-31 19:56:39 +0300

    4.4 Output matrix 2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n,m,r=int(input()),int(input()),[]
+n,m,r,q=int(input()),int(input()),[],[]
 for i in range(n):
     t=[]
     for j in range(m):
         t.append(input())
     r.append(' '.join(t))
+    q.append(t)
 print(*r,sep='\n')
+print('')
+print(*list(map(lambda x:' '.join(x),zip(*q))),sep='\n')
