46a8e7ac366af8fbcc4dd48eebbb1decb2419389
Jhonny <gonzodzen@mail.ru>
2024-01-31 22:31:09 +0300

    4.5 Exchange Columns

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 n,m=int(input()),int(input())
-r=[list(map(int,input().split())) for _ in range(n)]
-k,v=max(sum(r,[])),False
-for i in range(n):
-    if v:
-        break
-    for j in range(m):
-        if r[i][j]==k:
-            print(i,j)
-            v=True
-            break
+r=list(zip(*[list(map(int,input().split())) for _ in range(n)]))
+i,j=map(int,input().split())
+r[i],r[j]=r[j],r[i]
+[print(*i) for i in list(zip(*r))]
