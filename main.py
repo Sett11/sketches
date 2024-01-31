9890a62d04eeb037c52be1095a6201d14f2fd20d
Jhonny <gonzodzen@mail.ru>
2024-01-31 21:59:55 +0300

    4.5 Multiplication table

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-a=[list(map(int,input().split())) for _ in range(int(input()))]
-n=len(a)
-b=c=d=e=0
-for i in range(n):
-    for j in range(n):
-        t=a[i][j]
-        if i<j and i<n-1-j:
-            b+=t
-        if i<j and i>n-1-j:
-            c+=t
-        if i>j and i>n-1-j:
-            d+=t
-        if i>j and i<n-1-j:
-            e+=t
-print(f"""Верхняя четверть: {b}
-Правая четверть: {c}
-Нижняя четверть: {d}
-Левая четверть: {e}""")
+n,m=int(input()),int(input())
+r=[['0']*m for _ in range(n)]
+r[0]=' '.join(r[0]).ljust(m*3)
+for i in range(1,n):
+    for j in range(1,m):
+        r[i][j]=str(i*j)
+    r[i]=' '.join(r[i]).ljust(m*3)
+print(*r,sep='\n')
