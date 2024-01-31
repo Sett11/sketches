38a891e5e74af0db13072fb9ca38c49ef9ebbf30
Jhonny <gonzodzen@mail.ru>
2024-01-31 20:04:12 +0300

    4.4 Matrix trace

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-n,m,r,q=int(input()),int(input()),[],[]
-for i in range(n):
-    t=[]
-    for j in range(m):
-        t.append(input())
-    r.append(' '.join(t))
-    q.append(t)
-print(*r,sep='\n')
-print('')
-print(*list(map(lambda x:' '.join(x),zip(*q))),sep='\n')
+a=[list(map(int,input().split())) for _ in range(int(input()))]
+print(sum([a[i][i] for i in range(len(a))]))
