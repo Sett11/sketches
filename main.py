63fd9d9a1cb0c4c4192e0bcd6381d522885fe84f
Jhonny <gonzodzen@mail.ru>
2024-01-31 21:25:20 +0300

    4.4 Quarter amounts

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-a,m=[[-3, 1, 4, -3],
-     [-9, -3, -3, -10],
-     [-4, -3, -3, -2],
-     [-3, 0, 0, -3]],-100#[list(map(int,input().split())) for _ in range(int(input()))],-100
-n,k=len(a),1
-r,q=[],[]
+a=[list(map(int,input().split())) for _ in range(int(input()))]
+n=len(a)
+b=c=d=e=0
 for i in range(n):
-    t,w=[],[]
-    for j in range(k):
-        #m=max(a[i][j],a[i][::-1][j],m)
-        t.append(a[i][j])
-        w.append(a[i][::-1][j])
-    k=1 if i==n-2 else k+1 if i<n//2 and i!=n-1 else k-1
-    r.append(t)
-    q.append(w)
-print(r,q)
+    for j in range(n):
+        t=a[i][j]
+        if i<j and i<n-1-j:
+            b+=t
+        if i<j and i>n-1-j:
+            c+=t
+        if i>j and i>n-1-j:
+            d+=t
+        if i>j and i<n-1-j:
+            e+=t
+print(f"""Верхняя четверть: {b}
+Правая четверть: {c}
+Нижняя четверть: {d}
+Левая четверть: {e}""")
