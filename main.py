9e6fd83c77efb77e7f1897b2b4607f015294cdf0
Jhonny <gonzodzen@mail.ru>
2024-01-31 23:23:23 +0300

    4.5 Exchange of diagonals

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 a=[input().split() for _ in range(int(input()))]
-print(['NO','YES'][[list(i) for i in zip(*a)]==a])
+n,f=len(a),lambda x:[list(i) for i in zip(*x)]
+a=f(a)
+for i in range(n):
+    a[i][i],a[i][n-1-i]=a[i][n-1-i],a[i][i]
+print(*[' '.join(i) for i in f(a)],sep='\n')
