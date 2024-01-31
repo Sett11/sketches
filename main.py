07618f33c5c3d6daf5d9b7025d10fc18e17d2f50
Jhonny <gonzodzen@mail.ru>
2024-01-31 20:34:38 +0300

    4.4 Maximum in area 1

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-a=[list(map(int,input().split())) for _ in range(int(input()))]
-print(*[len([j for j in i if j>sum(i)/len(i)]) for i in a],sep='\n')
+a,m=[list(map(int,input().split())) for _ in range(int(input()))],float('-inf')
+for i in range(len(a)):
+    for j in range(i+1):
+        m=max(a[i][j],m)
+print(m)
