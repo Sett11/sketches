88a76a8550e539ea068cc4737c48980e7456eb2f
Jhonny <gonzodzen@mail.ru>
2024-02-01 08:27:40 +0300

    4.5 Knight moves

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(*[' '.join(i) for i in [i[::-1] for i in list(zip(*[input().split() for _ in range(int(input()))]))]],sep='\n')
+b=[['.']*8 for _ in range(8)]
+k,h=input()
+j,i='abcdefgh'.index(k),7-'12345678'.index(h)
+b[i][j]='N'
+for x,y in [[i+1,j+2],[i+2,j+1],[i+1,j-2],[i+2,j-1],[i-1,j-2],[i-2,j-1],[i-1,j+2],[i-2,j+1]]:
+    if 0<=x<8 and 0<=y<8:
+        b[x][y]='*'
+for w in b:
+    print(*w)
