ee7f61d388e9ad538846b62fd668481012e919e1
Jhonny <gonzodzen@mail.ru>
2024-02-01 21:12:40 +0300

    4.6 Filling 5 🌶️

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n=int(input())
-a=[['0']*n for _ in range(n)]
+n,m=map(int,input().split())
+r=list(range(1,m+1))
 for i in range(n):
-    for j in range(n):
-        if (i<=j and i<=n-1-j) or (i>=j and i>=n-1-j):
-            a[i][j]='1'
-print(*[' '.join(map(lambda x:x.ljust(3),i)) for i in a],sep='\n')
+    print(*r)
+    r.append(r.pop(0))
