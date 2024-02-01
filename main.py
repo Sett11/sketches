a8f51f224c8f0f3922d7b8f69b12f52d1c2a70c1
Jhonny <gonzodzen@mail.ru>
2024-02-01 20:33:42 +0300

    4.6 Filling 1

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-n=int(input())
-a=[['&']*n for _ in range(n)]
-for i in range(n):
-    h=n-1-i
-    a[i][h]=1
-    for j in range(h):
-        a[i][j]=0
-    for k in range(h+1,n):
-        a[i][k]=2
-    a[i]=' '.join(map(str,a[i]))
-print(*a,sep='\n')
+n,m=map(int,input().split())
+r=list(range(1,n*m+1))
+print(*[' '.join(map(lambda x:str(x).ljust(3),r[i:i+m])) for i in range(0,len(r),m)],sep='\n')
