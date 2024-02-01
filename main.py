e6e893aa2bac843f3a7d25076a4ec13a7bed549b
Jhonny <gonzodzen@mail.ru>
2024-02-01 07:38:43 +0300

    Mirror reflection

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-a=[input().split() for _ in range(int(input()))]
-n,f=len(a),lambda x:[list(i) for i in zip(*x)]
-a=f(a)
-for i in range(n):
-    a[i][i],a[i][n-1-i]=a[i][n-1-i],a[i][i]
-print(*[' '.join(i) for i in f(a)],sep='\n')
+print(*[input() for _ in range(int(input()))][::-1],sep='\n')
