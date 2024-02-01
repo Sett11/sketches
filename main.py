c3b0d6a32ace28bd6947e24fde2719065bd95ed2
Jhonny <gonzodzen@mail.ru>
2024-02-01 20:43:03 +0300

    4.6 Fill 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-n,m=map(int,input().split())
-r=list(range(1,n*m+1))
-print(*[' '.join(map(lambda x:str(x).ljust(3),r[i:i+m])) for i in range(0,len(r),m)],sep='\n')
+m,n=map(int,input().split())
+a=list(range(1,n*m+1))
+r=[' '.join(map(lambda x:str(x).ljust(3),j)) for j in zip(*[a[i:i+m] for i in range(0,len(a),m)])]
+print(*r,sep='\n')
