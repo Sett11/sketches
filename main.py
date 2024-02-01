b9214164fcff0efc96eaa73b53a874f0a8f59285
Jhonny <gonzodzen@mail.ru>
2024-02-01 20:06:19 +0300

    4.6 Chess board

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-n=int(input())
-a=[[int(j) for j in input().split()] for _ in range(n)]
-print(['NO','YES'][len(set([sum(i) for i in a]+[sum(i) for i in list(zip(*a))]+[sum([a[i][i] for i in range(n)])]+[sum([a[i][n-1-i] for i in range(n)])]))==1 and len(set(sum(a,[])))==n*n and all(i for i in sum(a,[]))])
+n,m=map(int,input().split())
+r=[['.']*m for _ in range(n)]
+k=1
+for i in range(n):
+    for j in range(k,m,2):
+        r[i][j]='*'
+    k=0 if k else 1
+    r[i]=' '.join(r[i])
+print(*r,sep='\n')
