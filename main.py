3aa8411b55ef9429a383d98673305177021ea509
Jhonny <gonzodzen@mail.ru>
2024-02-01 20:19:31 +0300

    4.6 Side diagonal

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-n,m=map(int,input().split())
-r=[['.']*m for _ in range(n)]
-k=1
+n=int(input())
+a=[['&']*n for _ in range(n)]
 for i in range(n):
-    for j in range(k,m,2):
-        r[i][j]='*'
-    k=0 if k else 1
-    r[i]=' '.join(r[i])
-print(*r,sep='\n')
+    h=n-1-i
+    a[i][h]=1
+    for j in range(h):
+        a[i][j]=0
+    for k in range(h+1,n):
+        a[i][k]=2
+    a[i]=' '.join(map(str,a[i]))
+print(*a,sep='\n')
