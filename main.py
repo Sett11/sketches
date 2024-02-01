17ef4772124bfef0b61c35277afb0074e349cdd1
Jhonny <gonzodzen@mail.ru>
2024-02-01 22:21:43 +0300

    4.6 Filling with diagonals 🌶️

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 n,m=map(int,input().split())
-a=list(range(1,n*m+1))
-r=[a[i:i+m] for i in range(0,n*m,m)]
-for i in range(n):
-    r[i]=' '.join(map(lambda x:str(x).ljust(3),r[i][::-1] if i&1 else r[i]))
-    print(r[i])
+a,r=list(range(n*m,0,-1)),[[0]*m for _ in range(n)]
+for i in range(n+m-1):
+    k,j=0 if i<m else i-m+1,i if i<m else m-1
+    while k<n and j>=0:
+        r[k][j]=a.pop()
+        k+=1
+        j-=1
+for i in r:
+    print(' '.join(map(lambda x:str(x).ljust(3),i)))
