78b84253c468000783e3be229c7fbd2afc66b38f
Jhonny <gonzodzen@mail.ru>
2024-02-02 16:27:35 +0300

    Every nth element

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n=int(input())
-a=b=[list(map(int,input().split())) for _ in range(n)]
-m=int(input())
-while m>1:
-    a=[[sum(a[i][j]*b[j][k] for j in range(n)) for k in range(n)] for i in range(n)]
-    m-=1
-for i in a:
-    print(*i)
+a,k,r=input().split(),int(input()),[]
+for i in range(k):
+    r.append(a[i::k])
+print(r)
