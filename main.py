01eeb13ff366c96caca10a35b3814b58880267dd
Jhonny <gonzodzen@mail.ru>
2024-02-02 16:40:33 +0300

    Matrix Transpose

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n=int(input())
-a,k=[list(map(int,input().split())) for _ in range(n)],int(-1e9)
-m=len(a[0])
-for i in range(n):
-    for j in range(m):
-        if i>=n-j-1:
-            k=max(a[i][j],k)
-print(k)
+a=list(zip(*[input().split() for _ in range(int(input()))]))
+for i in a:
+    print(*i)
