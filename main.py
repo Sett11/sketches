338b08495b4a8c46acf0b2ed1e2f41f80f7b8827
Jhonny <gonzodzen@mail.ru>
2024-02-02 17:33:53 +0300

    Queen moves

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-a=[input().split() for _ in range(int(input()))]
-b=[tuple(sorted(i)) for i in zip(*a)]
-c=[tuple(sorted(i)) for i in a]+b
-print(['NO','YES'][len(set(c))==1 and all(len(i)==len(set(i)) for i in c)])
+l,n,r,s='abcdefgh','12345678',[['.']*8 for _ in range(8)],input()
+a,b=7-n.index(s[1]),l.index(s[0])
+r[a]=['*']*8
+for  i in range(8):
+    r[i][b]='*'
+i,j=a,b
+while i>=0 and j>=0:
+    r[i][j]='*'
+    i-=1
+    j-=1
+i,j=a,b
+while i<8 and j<8:
+    r[i][j]='*'
+    i+=1
+    j+=1
+i,j=a,b
+while i<8 and j>=0:
+    r[i][j]='*'
+    i+=1
+    j-=1
+i,j=a,b
+while i>=0 and j<8:
+    r[i][j]='*'
+    i-=1
+    j+=1
+r[a][b]='Q'
+for i in [p for p in r]:
+    print(*i)
