036581d33a44f36fe8f4f4201005b1b7f2e644b7
Jhonny <gonzodzen@mail.ru>
2024-02-02 17:51:17 +0300

    Diagonals parallel to the main one

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-l,n,r,s='abcdefgh','12345678',[['.']*8 for _ in range(8)],input()
-a,b=7-n.index(s[1]),l.index(s[0])
-r[a]=['*']*8
-for  i in range(8):
-    r[i][b]='*'
-i,j=a,b
-while i>=0 and j>=0:
-    r[i][j]='*'
-    i-=1
-    j-=1
-i,j=a,b
-while i<8 and j<8:
-    r[i][j]='*'
-    i+=1
-    j+=1
-i,j=a,b
-while i<8 and j>=0:
-    r[i][j]='*'
-    i+=1
-    j-=1
-i,j=a,b
-while i>=0 and j<8:
-    r[i][j]='*'
-    i-=1
-    j+=1
-r[a][b]='Q'
-for i in [p for p in r]:
-    print(*i)
+n=int(input())
+r=[[0]*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i>j:
+            k,h=i,j
+            while k<n and h>=0:
+                r[k][h]=r[k-1][h]+1
+                k+=1
+                h-=1
+        if i<j:
+            k,h=i,j
+            while k>=0 and h<n:
+                r[k][h]=r[k][h-1]+1
+                k-=1
+                h+=1
+for i in range(n):
+    r[i][i]=0
+    print(*r[i])
