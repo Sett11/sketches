c097731eb7f4d3110e08137e350173984e2e7e3d
Jhonny <gonzodzen@mail.ru>
2024-02-02 22:14:59 +0300

    Vertex of a parabola

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-n=int(input())
-r=[[0]*n for _ in range(n)]
-
-for i in range(n):
-    for j in range(n):
-        if i>j:
-            k,h=i,j
-            while k<n and h>=0:
-                r[k][h]=r[k-1][h]+1
-                k+=1
-                h-=1
-        if i<j:
-            k,h=i,j
-            while k>=0 and h<n:
-                r[k][h]=r[k][h-1]+1
-                k-=1
-                h+=1
-for i in range(n):
-    r[i][i]=0
-    print(*r[i])
+a,b,c=int(input()),int(input()),int(input())
+print((-(b/(2*a)), (((4*a*c)-b**2))/4/a))
