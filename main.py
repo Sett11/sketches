451b205992c1ee1cb88482d4d5b9b916756e26fc
Jhonny <gonzodzen@mail.ru>
2024-02-02 17:00:19 +0300

    Latin square 🌶️

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-a=[input().split() for _ in range(int(input()))][::-1]
-b=[list(i) for i in zip(*a)]
-print(['NO','YES'][a==b])
+a=[input().split() for _ in range(int(input()))]
+b=[tuple(sorted(i)) for i in zip(*a)]
+c=[tuple(sorted(i)) for i in a]+b
+print(['NO','YES'][len(set(c))==1 and all(len(i)==len(set(i)) for i in c)])
