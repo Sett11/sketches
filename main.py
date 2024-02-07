00f286fc45d8cfca61fe69b4277c6a11ec210774
Jhonny <gonzodzen@mail.ru>
2024-02-07 23:00:01 +0300

    BEEGEEK 1 Online School

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-a,b=set(input().split()),set(input().split())
-c=' '.join(sorted(a&b,key=int,reverse=True))
-print(c if c else 'BAD DAY')
+print(['NO','YES'][set(input().split())==set(input().split())])
