435f040cd4b6e6e3a8c36f670f87e1ac4fb5233d
Jhonny <gonzodzen@mail.ru>
2024-02-07 23:36:09 +0300

    BEEGEEK 6 Online School 🌶️

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from functools import reduce as r
 q=[set(input() for __ in range(int(input()))) for _ in range(int(input()))]
-print(*sorted(r(lambda a,c:a.intersection(c),q)),sep='\n')
+print(*sorted(r(lambda a,c:a&c,q)),sep='\n')
