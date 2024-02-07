99f26820eb4f67b129f1ae7d1c2ad6578e2d2921
Jhonny <gonzodzen@mail.ru>
2024-02-07 18:05:10 +0300

    8.6 Number of matches

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-a=[]
-for i in map(int,input().split()):
-    if i not in a:
-        a.append(i)
-        print('NO')
-    else:
-        print('YES')
+a,b=input().split(),input().split()
+print(len(set(a)&set(b)))
