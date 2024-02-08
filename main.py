ccb41d2c7da734aba6f1b8b2850cdc8238e6f992
Jhonny <gonzodzen@mail.ru>
2024-02-08 22:42:32 +0300

    11.2 Scrabble game

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-d,r={},[]
-for i in input().split():
-    r.append(d.get(i,0)+1)
-    d[i]=1 if i not in d else d[i]+1
-print(*r)
+d={1:'AEILNORSTU',2:'DG',3:'BCMP',4:'FHVWY',5:'K',8:'JX',10:'QZ'}
+t,s=0,input()
+for i in s:
+    for j in d:
+        if i in d[j]:
+            t+=j
+            break
+print(t)
