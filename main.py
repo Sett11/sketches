3544f63de600a0d4926f7d9775b3c4a8113804f3
Jhonny <gonzodzen@mail.ru>
2024-02-09 00:22:59 +0300

    11.2 Dangerous virus 😈

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def merge(a):
-    r,d=set(sum([list(i.keys()) for i in a],[])),{}
-    for i in r:
-        t=set()
-        for j in a:
-            if i in j:
-                t.add(j[i])
-        d[i]=t
-    return d
-
-print(merge([{'a': 1, 'b': 2}, {'b': 10, 'c': 100}, {'a': 1, 'b': 17, 'c': 50}, {'a': 5, 'd': 777}]))
+d={}
+for i in range(int(input())):
+    a,*b=input().lower().split()
+    d[a]=b
+for i in range(int(input())):
+    a,b=input().lower().split()
+    print(['Access denied','OK'][('x' if a[0]=='e' else a[0]) in d[b]])
