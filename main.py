01f314d4fcd06294f19a53ef26de0100dc57d888
Jhonny <gonzodzen@mail.ru>
2024-02-08 21:29:30 +0300

    10.5

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
-result = {int(i):j for i,j in map(lambda x:x.split(':'),s.split())}
+f=lambda n:[i for i in range(1,n+1) if n%i==0]
+a = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+result = {i:f(i) for i in a}
 print(result)
