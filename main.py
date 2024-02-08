520025f02fcd2482f1561d235087084bdb04853d
Jhonny <gonzodzen@mail.ru>
2024-02-08 22:12:51 +0300

    11.1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-my_dict = {'C1': [10, 20, 30, 7, 6, 23, 90], 'C2': [20, 30, 40, 1, 2, 3, 90, 12], 'C3': [12, 34, 20, 21], 'C4': [22, 54, 209, 21, 7], 'C5': [2, 4, 29, 21, 19], 'C6': [4, 6, 7, 10, 55], 'C7': [4, 8, 12, 23, 42], 'C8': [3, 14, 15, 26, 48], 'C9': [2, 7, 18, 28, 18, 28]}
-for i in my_dict:
-    my_dict[i]=[j for j in my_dict[i] if j<=20]
+emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'], 
+          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'], 
+          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'], 
+          'yandex.ru': ['surface', 'google'],
+          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
+          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
+r=[]
+for i in emails:
+    t=[]
+    for j in emails[i]:
+        t.append(f'{j}@{i}')
+    r.extend(t)
+print(*sorted(r),sep='\n')
