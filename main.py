21263d5bcacaa80c22ba96b9db85e20313f6dcf2
Jhonny <gonzodzen@mail.ru>
2024-02-08 19:11:57 +0300

    10.3

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-a = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
-b = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
-
-result = {i:a[i]+b[i] if i in a and i in b else a.get(i,b.get(i)) for i in set(list(a.keys())+list(b.keys()))}
-print(result)
+from collections import Counter
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+result = Counter(text)
