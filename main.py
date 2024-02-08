7ba6852e721163e1422af612219ac21e5778f9f1
Jhonny <gonzodzen@mail.ru>
2024-02-08 20:10:33 +0300

    10.3 Anagrams 1

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-d={}
-for i in range(int(input())):
-    a,b=input().split(': ')
-    d[a.lower()]=b
-for i in range(int(input())):
-    print(d.get(input().lower(),'Не найдено'))
+print(['NO','YES'][sorted(input())==sorted(input())])
