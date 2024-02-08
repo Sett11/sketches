7886ef32de26694397f7969ab854ee08edac02f7
Jhonny <gonzodzen@mail.ru>
2024-02-08 23:10:47 +0300

    11.2 Merging dictionaries 🌶️

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-build_query_string = lambda a:'&'.join(map(lambda x:f'{x[0]}={x[1]}',sorted(a.items())))
+def merge(a):
+    r,d=set(sum([list(i.keys()) for i in a],[])),{}
+    for i in r:
+        t=set()
+        for j in a:
+            if i in j:
+                t.add(j[i])
+        d[i]=t
+    return d
 
-print(build_query_string({'sport': 'hockey', 'game': 2, 'time': 17}))
+print(merge([{'a': 1, 'b': 2}, {'b': 10, 'c': 100}, {'a': 1, 'b': 17, 'c': 50}, {'a': 5, 'd': 777}]))
