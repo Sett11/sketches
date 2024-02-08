14292df1d923fef547dc3dd448a2f509e4c5683a
Jhonny <gonzodzen@mail.ru>
2024-02-08 21:34:53 +0300

    10.5

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-a = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
-result = {i:[ord(j) for j in i] for i in a}
+a = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 26: 'Z'}
+
+b = [1, 5, 7, 12, 17, 19, 21, 24]
+
+result = {i:a[i] for i in a if i not in b}
