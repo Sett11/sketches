fe737cc751bbda4d65f466ea6bd42bbad84d58bf
Jhonny <gonzodzen@mail.ru>
2024-02-08 19:01:54 +0300

    10.3

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-result = {i:i**2 for i in range(1,16)}
+a = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+b = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+result = {i:a[i]+b[i] if i in a and i in b else a.get(i,b.get(i)) for i in set(list(a.keys())+list(b.keys()))}
 print(result)
