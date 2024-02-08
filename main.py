0787c0f66d2038b8877c0fbe2b24962182a5d596
Jhonny <gonzodzen@mail.ru>
2024-02-08 22:56:43 +0300

    11.2 Query string

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-d={1:'AEILNORSTU',2:'DG',3:'BCMP',4:'FHVWY',5:'K',8:'JX',10:'QZ'}
-t,s=0,input()
-for i in s:
-    for j in d:
-        if i in d[j]:
-            t+=j
-            break
-print(t)
+build_query_string = lambda a:'&'.join(map(lambda x:f'{x[0]}={x[1]}',sorted(a.items())))
+
+print(build_query_string({'sport': 'hockey', 'game': 2, 'time': 17}))
