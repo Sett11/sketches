f2e01b617472ca6fbc605b0b6b757ce457fbf09c
Jhonny <gonzodzen@mail.ru>
2024-02-08 22:20:38 +0300

    A minute of genetics

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'], 
-          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'], 
-          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'], 
-          'yandex.ru': ['surface', 'google'],
-          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
-          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
-r=[]
-for i in emails:
-    t=[]
-    for j in emails[i]:
-        t.append(f'{j}@{i}')
-    r.extend(t)
-print(*sorted(r),sep='\n')
+print(input().translate(str.maketrans('GCTA','CGAU')))
