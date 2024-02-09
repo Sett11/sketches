fb8fed3d6e4d08958f3f35483b3d481f78a9c5d1
Jhonny <gonzodzen@mail.ru>
2024-02-09 20:20:47 +0300

    12.3

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from random import uniform
 
 n=10**6
 k=0
-S0=2**4
 
 for i in range(n):
-    x=uniform(-2,2)
-    y=uniform(-2,2)
-    if x**3+y**4+2>=0 and 3*x+y**2<=2:
+    x=uniform(-1,1)
+    y=uniform(-1,1)
+    if x**2+y**2<=1:
         k+=1
 
-print((k/n)*S0)
+print(k/n*4)
