079d590b9ccfd84f64ea9112b8a86cf361168493
Jhonny <gonzodzen@mail.ru>
2024-02-09 15:52:49 +0300

    12.1

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-d={}
+from random import random
+
 for _ in range(int(input())):
-    a,*b=input().split()
-    if a in d:
-        if b[0] in d[a]:
-            d[a][b[0]]+=int(b[1])
-        else:
-            d[a][b[0]]=int(b[1])
-    else:
-        d[a]={b[0]:int(b[1])}
-for i in sorted(d):
-    t=sorted(d[i])
-    print(f'{i}:')
-    for j in t:
-        print(j,d[i][j])
+    print(['Орел','Решка'][random()>0.5])
