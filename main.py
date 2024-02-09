ae541a1a9de2916f7167139a7f0174a43eb1cadb
Jhonny <gonzodzen@mail.ru>
2024-02-09 16:29:50 +0300

    12.2

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from random import shuffle as h
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16]]
-h(list(map(lambda x:h(x),matrix)))
-print(matrix)
+from random import randint
+print(*[randint(1000000,9999999) for _ in range(100)],sep='\n')
