9b1069256f1660efde066a16fdffb24a1da933ce
Jhonny <gonzodzen@mail.ru>
2024-02-09 16:27:08 +0300

    12.2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from random import randint as r
-from string import ascii_uppercase as a
-generate_index=lambda:'{0}{1}{2}_{2}{1}{0}'.format(a[r(0,25)],a[r(0,25)],r(0,99))
-
-print(generate_index())
+from random import shuffle as h
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+h(list(map(lambda x:h(x),matrix)))
+print(matrix)
