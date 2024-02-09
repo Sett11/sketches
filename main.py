b01fd83f3dc8b77aad50e91bbfe59407ccc63f47
Jhonny <gonzodzen@mail.ru>
2024-02-09 16:24:00 +0300

    12.2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint
-generate_ip=lambda:'.'.join(str(randint(0,255)) for _ in range(4))
+from random import randint as r
+from string import ascii_uppercase as a
+generate_index=lambda:'{0}{1}{2}_{2}{1}{0}'.format(a[r(0,25)],a[r(0,25)],r(0,99))
 
-print(generate_ip())
+print(generate_index())
