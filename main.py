a3abf38f53d4b5d18457d549e5ddad11085084a4
Jhonny <gonzodzen@mail.ru>
2024-02-09 20:07:51 +0300

    12.3

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from string import digits as d, ascii_uppercase as a
-from random import choice,sample
-from re import sub
-a=sub(r'[IO]','',a)
-f,n,m=lambda n:'a'+''.join(sample(a,n-1)[:-1])+choice(d[2:]),int(input()),int(input())
-print(*[f(m) for _ in range(n)],sep='\n')
+from random import uniform
+
+n=10**6
+k=0
+S0=2**4
+
+for i in range(n):
+    x=uniform(-2,2)
+    y=uniform(-2,2)
+    if x**3+y**4+2>=0 and 3*x+y**2<=2:
+        k+=1
+
+print((k/n)*S0)
