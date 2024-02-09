2d9ebb749a99d801d80a76a03ad2e53c8063ffa2
Jhonny <gonzodzen@mail.ru>
2024-02-09 17:16:50 +0300

    12.2 Password generator 2 🌶️

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from string import digits as d, ascii_letters as a
+from string import digits as d, ascii_uppercase as a
 from random import choice,sample
 from re import sub
-a=sub(r'[lIoO]','',a)
-f,n,m=lambda n: ''.join(sample(a,n)[:-1])+choice(d[2:]),int(input()),int(input())
+a=sub(r'[IO]','',a)
+f,n,m=lambda n:'a'+''.join(sample(a,n-1)[:-1])+choice(d[2:]),int(input()),int(input())
 print(*[f(m) for _ in range(n)],sep='\n')
