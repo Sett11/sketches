4fff3b9044c0a377956f6c402cc8570cfe8b6084
Jhonny <gonzodzen@mail.ru>
2024-02-09 16:37:39 +0300

    12.2

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-from random import randint
-print(*[randint(1000000,9999999) for _ in range(100)],sep='\n')
+from random import sample
+a=sample(range(1,76),25)
+a[12]=0
+print(*[''.join(map(lambda s:str(s).ljust(3),a[i:i+5])) for i in range(0,25,5)],sep='\n')
