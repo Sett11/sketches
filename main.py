91c2338370c9104888e96f9a33f96958662bc956
Jhonny <gonzodzen@mail.ru>
2024-02-12 15:11:02 +0300

    13.1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-s=input().replace('.','').replace('-','')
-print(int(max(s))+int(min(s)))
+from decimal import *
+d = Decimal(input())
+def e(num):
+    return num.exp()
+def sqrt(num):
+    return num.sqrt()
+def ln(num):
+    return num.ln()
+def lg(num):
+    return num.log10()
+print(e(d) + ln(d) + lg(d) + sqrt(d))
