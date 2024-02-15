3d5d02d1cf0381f800590394ac7ee06c42d5b856
Jhonny <gonzodzen@mail.ru>
2024-02-15 15:41:06 +0300

    13.2 Sum of fractions 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fractions import Fraction as f
-n,t=int(input()),f(1,1**2)
+from math import factorial as a
+n,t=int(input()),f(1,a(1))
 for i in range(2,n+1):
-    t+=f(1,i**2)
+    t+=f(1,a(i))
 print(t)
