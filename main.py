7cc49b07360cc96713df48903da2538f19fcc517
Jhonny <gonzodzen@mail.ru>
2024-02-16 18:24:04 +0300

    13.2 Ordered fractions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from math import gcd
+from fractions import Fraction
 n=int(input())
 r=[]
-for i in range(1,n):
-    for j in range(i+1,n):
-        if gcd(i,j)==1 and i+j==n:
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if gcd(i,j)==1 and (i/j<1 or j/i<1) and i<j:
             r.append((i,j))
-print(f'{r[-1][0]}/{r[-1][1]}')
+print(*sorted(filter(lambda y:y%1!=0,set(map(lambda x:Fraction(x[0],x[1]),r)))),sep='\n')
