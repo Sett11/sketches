83b9ab21f905dae1c47d59f2ad9d81435f0f4553
Jhonny <gonzodzen@mail.ru>
2024-02-20 00:03:25 +0300

    Conspect

--- main.py
+++
@@ -1,9 +0,0 @@
-from math import gcd
-from fractions import Fraction
-n=int(input())
-r=[]
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if gcd(i,j)==1 and (i/j<1 or j/i<1) and i<j:
-            r.append((i,j))
-print(*sorted(filter(lambda y:y%1!=0,set(map(lambda x:Fraction(x[0],x[1]),r)))),sep='\n')
