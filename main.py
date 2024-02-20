21d534e54822334b2b0f756c0d25a071f9711fe6
Jhonny <gonzodzen@mail.ru>
2024-02-20 19:55:00 +0300

    13.3 Conjugate numbers

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-a = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
-a.sort(key=abs)
-print(a[-1],abs(a[-1]),sep='\n')
+a,b,c=int(input()),complex(input()),complex(input())
+print(b**a+c**a+b.conjugate()**a+c.conjugate()**(a+1))
