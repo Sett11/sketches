ecc224091f2432c2255cd0b1bb5f4031fb02abec
Jhonny <gonzodzen@mail.ru>
2024-02-20 22:56:18 +0300

    15.2

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def print_products(*args):
-    a=[f'{i+1}) {j}' for i,j in enumerate([k for k in args if k and isinstance(k,str)])]
-    print(*a if a else 'Нет продуктов',sep='\n' if a else '')
+def info_kwargs(**kwargs):
+    print(*map(lambda s:f'{s[0]}: {s[1]}',sorted(kwargs.items())),sep='\n')
 
-print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
-print_products([4], {}, 1, 2, {'Beegeek'}, '') 
+info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
