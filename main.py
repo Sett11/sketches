baf41f2e464d3f99dc70e1f751db11707cf77aa4
Jhonny <gonzodzen@mail.ru>
2024-02-20 22:24:25 +0300

    15.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-sq_sum=lambda *args:sum(map(lambda x:x**2,args))
+mean=lambda *args,f=lambda a:list(filter(lambda x:type(x) in (int,float),a)):sum(f(args))/(len(f(args)) or 1)
 
-print(sq_sum(1,2,3,4,5))
+print(mean(1.5, True, ['stepik'], 'beegeek', 2.5))
+print(mean(True,['stepik'], 'beegeek'))
