859e6122ea1d97fe1d902d413458fb55b5b1b3e8
Jhonny <gonzodzen@mail.ru>
2024-02-20 22:50:55 +0300

    15.2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-greet=lambda s,*args:'Hello, '+' and '.join([s]+list(args))+'!'
+def print_products(*args):
+    a=[f'{i+1}) {j}' for i,j in enumerate([k for k in args if k and isinstance(k,str)])]
+    print(*a if a else 'Нет продуктов',sep='\n' if a else '')
 
-print(greet('beegeek'))
-print(greet('stepik', 'beegeek'))
-print(greet('beegeek','a','b'))
+print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
+print_products([4], {}, 1, 2, {'Beegeek'}, '') 
