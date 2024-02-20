c73bbc7e27682ac88e495f8c80612fee27158933
Jhonny <gonzodzen@mail.ru>
2024-02-20 19:37:43 +0300

    13.3

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-a,b=complex(input()),complex(input())
-print(f'{a} + {b} = {a+b}',f'{a} - {b} = {a-b}',f'{a} * {b} = {a*b}',sep='\n')
+a = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+a.sort(key=abs)
+print(a[-1],abs(a[-1]),sep='\n')
