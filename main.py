d0f6f955e560ade8a3656b5ff8dad201d672ed7d
Jhonny <gonzodzen@mail.ru>
2024-02-21 21:09:48 +0300

    15.8

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-func=lambda s:len(set((s[0]+s[-1]).lower()))==1 and s[0] in 'Aa'
+is_non_negative_num=lambda s:bool(__import__('re').match(r'^\d*\.?\d*?$',s))
 
-print(func('Abbba'))
+print(is_non_negative_num('0'))
