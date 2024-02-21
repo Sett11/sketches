c8d6cfb6e99390e0698a59b0d26e35a6555ede3e
Jhonny <gonzodzen@mail.ru>
2024-02-21 23:17:10 +0300

    15.9 Inside the ball

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
-capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
-population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
-print(*map(lambda s:f'{s[0]} is the capital of {s[1]}, population equal {s[2]} people.',zip(capitals,countries,population)),sep='\n')
+from decimal import Decimal as d
+print(all(x[0]**2+x[1]**2+x[2]**2<=4 for x in zip([d(i) for i in input().split()],[d(i) for i in input().split()],[d(i) for i in input().split()])))
