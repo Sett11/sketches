b1a39194aee7317ba787422901cbbecd11ac2169
Jhonny <gonzodzen@mail.ru>
2024-02-21 18:48:49 +0300

    15.4 Interesting sorting-1

--- main.py
+++ main.py
@@ -1,2 +1,1 @@
-n=int(input())
-print({'квадрат':lambda x:x**2,'корень':lambda x:x**.5,'куб':lambda x:x**3,'модуль':abs,'синус':__import__('math').sin}[input()](n))
+print(*sorted(input().split(),key=lambda x:sum(map(int,x))))
