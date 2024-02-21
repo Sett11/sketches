c70fe09067cce19415ba170b5cb52efd68cef60a
Jhonny <gonzodzen@mail.ru>
2024-02-21 19:11:03 +0300

    15.5

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print(*sorted(input().split(),key=lambda x:(sum(map(int,x)),int(x))))
+print(*map(lambda x:round(x,2),[3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]),sep='\n')
