41b17665bda495c1105c91c0c32ae7c4c011d2f2
Jhonny <gonzodzen@mail.ru>
2024-02-21 21:13:31 +0300

    15.8

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-is_non_negative_num=lambda s:bool(__import__('re').match(r'^\d*\.?\d*?$',s))
+is_num=lambda s:bool(__import__('re').match(r'^\-?\d*\.?\d*?$',s))
 
-print(is_non_negative_num('0'))
+print(is_num('-10'))
