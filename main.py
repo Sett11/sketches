90b55b776ca90d8da39235a0f19b03d2e610605a
Jhonny <gonzodzen@mail.ru>
2024-02-21 23:20:38 +0300

    15.9 Correct IP address

--- main.py
+++ main.py
@@ -1,2 +1,1 @@
-from decimal import Decimal as d
-print(all(x[0]**2+x[1]**2+x[2]**2<=4 for x in zip([d(i) for i in input().split()],[d(i) for i in input().split()],[d(i) for i in input().split()])))
+print(all(i.isdigit() and int(i)<256 for i in input().split('.')))
