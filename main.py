8be9b6524fb46ac0d0e11bf374f7113476b70202
Jhonny <gonzodzen@mail.ru>
2024-02-21 07:59:34 +0300

    15.4

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def info_kwargs(**kwargs):
-    print(*map(lambda s:f'{s[0]}: {s[1]}',sorted(kwargs.items())),sep='\n')
-
-info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
+a = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+f=lambda x:sum(x)/len(x)
+print(min(a,key=f),max(a,key=f),sep='\n')
