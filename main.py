56b7e65e667af3823617c9e72987ab9d61267803
Jhonny <gonzodzen@mail.ru>
2024-02-22 20:10:48 +0300

    16.3

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-words = 'the world is mine take a look what you have started'.split()
-
-print(*map(lambda s:f'"{s}"',words))
+numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
+print(*filter(lambda x:str(x)!=str(x)[::-1],numbers))
