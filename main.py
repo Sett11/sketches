901272ac8000f0b3fe054cc0a203f6ce3d6107e3
Jhonny <gonzodzen@mail.ru>
2024-02-22 19:15:45 +0300

    Letter for the exam

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(['NO','YES'][all('5' in k for k in [[input().split()[1] for _ in range(int(input()))] for __ in range(int(input()))])])
+def generate_letter(mail, name, date, time, place,teacher='Тимур Гуев',number=17):
+    return """To: {0}
+    Приветствую, {1}!
+    Вам назначен экзамен, который пройдет {2}, в {3}.
+    По адресу: {4}. 
+    Экзамен будет проводить {5} в кабинете {6}. 
+    Желаем удачи на экзамене!""".format(mail, name, date, time, place,teacher,number)
+
+print(generate_letter(1,2,3,4,5))
