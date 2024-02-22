8ae2b7e79e0b57cbade2c8b8fe5b2b8a4ce58afb
Jhonny <gonzodzen@mail.ru>
2024-02-22 20:20:19 +0300

    16.3

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def call(f,*args):
-    return f(*args)
+def compose(f,g):
+    return lambda x:f(g(x))
+
+def add3(x):
+    return x + 3
+
+
+def mul7(x):
+    return x * 7
+
+print(compose(mul7, add3)(1))
