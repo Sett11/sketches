6955e3876d4d8fa90ce1789f0263118229bb8d8f
Jhonny <gonzodzen@mail.ru>
2024-02-22 20:25:36 +0300

    16.3 One line

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-arithmetic_operation =lambda x:lambda a,b:eval(f'{a}{x}{b}')
-
-add = arithmetic_operation('+')
-div = arithmetic_operation('/')
-print(add(10, 20))
-print(div(20, 5))
+print(*sorted(input().split(),key=str.lower))
