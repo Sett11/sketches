e3d02c2319b32f2ef902fdd73141944bffaa5ee2
Jhonny <gonzodzen@mail.ru>
2024-02-23 21:04:12 +0300

    17.3 Sum of numbers in lines

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-a,m=[],0
-with open('lines.txt', encoding='utf-8') as file:
+with open('numbers.txt', encoding='utf-8') as file:
     for i in file:
-        s=i.strip()
-        m=max(m,len(s))
-        a.append(s)
-print(*[i for i in a if len(i)==m],sep='\n')
+        print(sum(map(int,i.split())))
+        
