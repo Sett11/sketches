331c4f0ce28f4171596fca495ef215777a79c3ef
Jhonny <gonzodzen@mail.ru>
2024-02-23 22:37:48 +0300

    17.4 Input string

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-def read_csv():
-    a=[]
-    with open('data.csv',encoding='utf-8') as f:
-        for i in f.readlines():
-            a.append(list(map(str.strip,i.split(','))))
-    b=a.pop(0)
-    for i in a:
-        for j in range(len(i)):
-            i[j]=(b[j],i[j])
-    return list(map(dict,a))
-
-print(read_csv())
+with open('output.txt','w',encoding='utf-8') as f:
+    f.write(input())
