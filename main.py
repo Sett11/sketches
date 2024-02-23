6597b69184b7ef1151c43a68b44aa3726789660f
Jhonny <gonzodzen@mail.ru>
2024-02-23 22:41:14 +0300

    17.4 Random numbers

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-with open('output.txt','w',encoding='utf-8') as f:
-    f.write(input())
+from random import randint
+with open('random.txt','w',encoding='utf-8') as f:
+    for i in range(25):
+        f.write(str(randint(111,777))+'\n')
