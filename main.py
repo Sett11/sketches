753203f7ce61d5f7c9d95c854b0ed0390b35bf7e
Jhonny <gonzodzen@mail.ru>
2024-02-23 22:18:01 +0300

    17.3 CSV-file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-with open('population.txt',encoding='utf-8') as f:
-    for i in f.readlines():
-        a,b=i.split('\t')
-        if a[0]=='G' and int(b.strip())>5e5:
-            print(a)
+def read_csv():
+    a=[]
+    with open('data.csv',encoding='utf-8') as f:
+        for i in f.readlines():
+            a.append(list(map(str.strip,i.split(','))))
+    b=a.pop(0)
+    for i in a:
+        for j in range(len(i)):
+            i[j]=(b[j],i[j])
+    return list(map(dict,a))
+
+print(read_csv())
