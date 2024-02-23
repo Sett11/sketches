7099334a8b77962c96c671e5dd6f1196953feb5e
Jhonny <gonzodzen@mail.ru>
2024-02-23 21:34:35 +0300

    17.3 File statistics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from re import split
-with open('nums.txt', encoding='utf-8') as file:
-    print(sum([int(i) for i in split(r'[^\d]',file.read()) if i.isdigit()]))
-        
+from re import sub
+a=[]
+
+with open('file.txt', encoding='utf-8') as file:
+    a=list(map(str.strip,file.readlines()))
+print('Input file contains:',f"{len(''.join([sub(r'[^A-z]','',i) for i in a]))} letters",f'{sum([len(i.split()) for i in a])} words',f'{len(a)} lines',sep='\n')
