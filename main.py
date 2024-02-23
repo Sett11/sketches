ec54774864435b34a7f4550a562b8a88f0ecdbf8
Jhonny <gonzodzen@mail.ru>
2024-02-23 21:10:18 +0300

    17.3 Sum of numbers in file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open('numbers.txt', encoding='utf-8') as file:
-    for i in file:
-        print(sum(map(int,i.split())))
+from re import split
+with open('nums.txt', encoding='utf-8') as file:
+    print(sum([int(i) for i in split(r'[^\d]',file.read()) if i.isdigit()]))
         
