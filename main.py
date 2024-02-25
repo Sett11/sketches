b8acd973981c6a2c311e2d1319eb6e777cb09666
Jhonny <gonzodzen@mail.ru>
2024-02-25 14:10:45 +0300

    17.4 Riddle from Jacques Fresco 🌶️

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-with open('logfile.txt','r',encoding='utf-8') as h,open('output.txt','w',encoding='utf-8') as o:
+from collections import Counter as c
+with open('goats.txt','r',encoding='utf-8') as h,open('answer.txt','w',encoding='utf-8') as o:
+    v,a=False,[]
     for i in h:
-        a,b,c=i.split(', ')
-        if abs(float(b.replace(':','.'))-float(c.replace(':','.')))>=1:
-            print(a)
+        i=i.strip()
+        if v:
+            a.append(i)
+        if i=='GOATS':
+            v=True
+    d=c(a)
+    for i in d:
+        if d[i]>(len(a)/100*7):
+            o.write(i+'\n')
