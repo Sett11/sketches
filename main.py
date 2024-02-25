4a257a9768e8087981b1e84ca40d3625967712af
Jhonny <gonzodzen@mail.ru>
2024-02-25 15:54:48 +0300

    Forbidden words 🌶️

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-filename = input()
-with open(filename, encoding='utf-8') as file:
-    content = file.readlines()
-    count = len(content)
-    if count < 10:
-        for s in content:
-            print(s, end='')
-    else:
-        res = content[-10:]
-        for s in res:
-            print(s, end='')
+from re import sub,IGNORECASE
+s=set()
+with open('forbidden_words.txt','r',encoding='utf-8') as d:
+    s.update(d.read().split())
+with open(input()) as x:
+    a=x.read()
+    for i in s:
+        a=sub(i,lambda c:'*'*len(c.group()),a,flags=IGNORECASE)
+    print(a)
