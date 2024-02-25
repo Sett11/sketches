a22c9acdaadf1e9f01517a8f502d14923a31a369
Jhonny <gonzodzen@mail.ru>
2024-02-25 16:17:31 +0300

    Missed comments 🌶️

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from string import printable as p
-d={
-    'а': 'a', 'к': 'k', 'х': 'h', 'б': 'b', 'л': 'l', 'ц': 'c', 'в': 'v', 'м': 'm', 'ч': 'ch',
-    'г': 'g', 'н': 'n', 'ш': 'sh', 'д': 'd', 'о': 'o', 'щ': 'shh', 'е': 'e', 'п': 'p', 'ъ': '*',
-    'ё': 'jo', 'р': 'r', 'ы': 'y', 'ж': 'zh', 'с': 's', 'ь': "'", 'з': 'z', 'т': 't', 'э': 'je',
-    'и': 'i', 'у': 'u', 'ю': 'ju', 'й': 'j', 'ф': 'f', 'я': 'ya',
-    'А': 'A', 'К': 'K', 'Х': 'H', 'Б': 'B', 'Л': 'L', 'Ц': 'C', 'В': 'V', 'М': 'M', 'Ч': 'Ch',
-    'Г': 'G', 'Н': 'N', 'Ш': 'Sh', 'Д': 'D', 'О': 'O', 'Щ': 'Shh', 'Е': 'E', 'П': 'P', 'Ъ': '*',
-    'Ё': 'Jo', 'Р': 'R', 'Ы': 'Y', 'Ж': 'Zh', 'С': 'S', 'Ь': "'", 'З': 'Z', 'Т': 'T', 'Э': 'Je',
-    'И': 'I', 'У': 'U', 'Ю': 'Ju', 'Й': 'J', 'Ф': 'F', 'Я': 'Ya',
-}
-with open('cyrillic.txt', encoding='utf-8') as c,open('transliteration.txt','w',encoding='utf-8') as f:
-    for i in c:
-        for j in i:
-            if j in p:
-                print(j,end='')
-            else:
-                print(d[j],end='')
+f=input()
+a,r=[],[]
+with open(f,encoding='utf-8') as f:
+    for i in f:
+        t=i.strip()
+        if t:
+            a.append(t)
+if a[0].startswith('def'):
+    r.append(a[0].replace('def ',''))
+    r[0]=r[0][:r[0].index('(')]
+for i in range(1,len(a)):
+    if a[i].startswith('def') and not a[i-1].startswith('#'):
+        t=a[i].replace('def ','')
+        t=t[:t.index('(')]
+        r.append(t)
+if r:
+    print(*r,sep='\n')
+else:
+    print('Best Programming Team')
