6c73ce9f8b1143a8e692ddf37886db09b950e99b
Jhonny <gonzodzen@mail.ru>
2024-02-25 14:36:48 +0300

    17.4 File concatenation 🌶️

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from collections import Counter as c
-with open('goats.txt','r',encoding='utf-8') as h,open('answer.txt','w',encoding='utf-8') as o:
-    v,a=False,[]
-    for i in h:
-        i=i.strip()
-        if v:
-            a.append(i)
-        if i=='GOATS':
-            v=True
-    d=c(a)
-    for i in d:
-        if d[i]>(len(a)/100*7):
-            o.write(i+'\n')
+for _ in range(int(input())):
+    with open(input(),'r') as f,open('output.txt','a') as o:
+        o.write(f.read())
