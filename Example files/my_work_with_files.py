9d6d3e967cd0091ced7c3eaa20b12054635a7f97
Jhonny <gonzodzen@mail.ru>
2024-02-28 00:01:27 +0300

    files_info

---
+++ Example files/my_work_with_files.py
@@ -0,0 +1,12 @@
+import os
+# from string import ascii_letters,digits
+# from random import randint,sample,shuffle
+
+# def generate_random_list():
+#     a=list(ascii_letters+digits)
+#     shuffle(a)
+#     return [''.join(sample(a,randint(1,20))) for _ in range(randint(1,20))]
+
+# a=generate_random_list()
+
+os.replace('text.txt','Example files\\text.txt')
