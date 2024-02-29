370fcfed42ebaad852a010ecb72927ff32f0397b
Jhonny <gonzodzen@mail.ru>
2024-02-29 08:24:15 +0300

    2.2 Area of ​​a rhombus

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-a,b,c=int(input()),int(input()),int(input())
-p=(a+b+c)
-pp=p/2
-print(p,(pp*((pp-a)*(pp-b)*(pp-c)))**.5,sep='\n')
+a,b=int(input()),int(input())
+p=(a*b)/2
+print(int(p),round(p,1),p,sep='\n')
