0b6071c6f4e674797fb7b1b45007a995f2099863
Jhonny <gonzodzen@mail.ru>
2024-03-02 22:09:32 +0300

    2.5

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-n=int(input())
-if not n:
-    print('Division by zero!')
+s=input()
+if s in ['list','str','int']:
+    if s=='int':
+        a,b=int(input()),int(input())
+        if not a and not b:
+            print("Empty Ints")
+        else:
+            print(a+b)
+    elif s=='str':
+        c=input()
+        print(c or "Empty String")
+    else:
+        a=input().split()
+        print(a[-1] if a else "Empty List")
 else:
-    print(round(int(input())/n,1))
+    print("Unknown type")
