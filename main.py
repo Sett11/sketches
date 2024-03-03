6872e3b486c992d782d025fa952f02a76064cf44
Jhonny <gonzodzen@mail.ru>
2024-03-03 21:50:38 +0300

    4.2 kaprekar_loop(n) with input data validation

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ def kaprekar_step(L):
     return int(''.join(map(str,L[::-1])))-int(''.join(map(str,L)))
 
 def kaprekar_loop(n):
+    if n==1000:
+        print("Ошибка! На вход подано число 1000")
+        return
+    if len(set(numerics(n)))==1:
+        print(f"Ошибка! На вход подано число {n} - все цифры одинаковые")
+        return
     print(n)
     while n!=6174:
         n=kaprekar_step(numerics(n))
         print(n)
 
-print(kaprekar_loop(8654))
+print(kaprekar_loop(1000))
