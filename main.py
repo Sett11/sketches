b81bf305f026620f6d7819b9ae3a91092cbf14f5
Jhonny <gonzodzen@mail.ru>
2024-03-03 19:17:05 +0300

    2.8

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-with open('output.txt','w',encoding='utf-8') as f:
-    f.write(input())
+import os
+
+event,file_name='aaa','txt.log'
+x=''
+
+if os.path.isfile(file_name):
+    with open(file_name,'r',encoding='utf-8') as f:
+        x=f.read()
+
+with open(file_name,'w',encoding='utf-8') as f:
+    if x:
+        n=int(x.split()[1])+1
+    else:
+        n=1
+    f.seek(0)
+    f.write(f"event {n} - '{event}'\n"+x)
