afbdd916fc917480d98126797871727b8807e594
Jhonny <gonzodzen@mail.ru>
2024-03-03 22:20:17 +0300

    4.3

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-def numerics(n):
-    return list(map(int,str(n)))
+def luka(a,b,n):
+    while n:
+        a,b=b,a+b
+        n-=1
+    return a
 
-def kaprekar_check(n):
-    s=str(n)
-    return len(s) in (3,4,6) and len(set(s))!=1 and n not in [100,1000,100000]
-
-def kaprekar_step(L):
-    L.sort()
-    return int(''.join(map(str,L[::-1])))-int(''.join(map(str,L)))
-
-
-def kaprekar_loop(n):
-    if not kaprekar_check(n):
-        print(f"Ошибка! На вход подано число {n}, не удовлетворяющее условиям процесса Капрекара")
-        return
-    r=[]
-    print(n)
-    while n not in (495, 6174, 549945, 631764):
-        n=kaprekar_step(numerics(n))
-        if n in r:
-            print(f'Следующее число - {n}, кажется процесс зациклился...')
-            return
-        r.append(n)
-        print(n)
-
-print(kaprekar_loop(1000))
+print(luka(12345,67890,5))
