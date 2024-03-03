352dcbf2ee07c57b0880db8f973f5459fca0c2d3
Jhonny <gonzodzen@mail.ru>
2024-03-03 18:13:22 +0300

    2.8

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-with open(input(),'r',encoding='utf-8') as f:
-    print(f.readlines()[-2])
+r=[]
+with open(sheet,'r',encoding='utf-8') as f:
+    for i in f.readlines():
+        p=i.split()
+        if p[-1]=='(экзамен)' or p[-1]=='(автомат)':
+            r.append(int(p[-2]))
+t=sum(r)/len(r)
+with open(mean,'r',encoding='utf-8') as f:
+    x=sum(map(int,f.read().strip()))
+    if x==t:
+        print('OK')
+    else:
+        print('ERROR')
