9a9e25b6c04820e50953fb122059db73b7e4677a
Jhonny <gonzodzen@mail.ru>
2024-03-03 15:53:55 +0300

    2.7

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-convert=lambda a:list(map(int,a))
+from string import digits as d
+def translate(n,b=2):
+    r=''
+    while n:
+        r=d[n%b]+r
+        n//=b
+    return r
+
+print(translate(19))
