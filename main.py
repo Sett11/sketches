c0ab249be4658f0a7e2bcb467007e6a8799aedb5
Jhonny <gonzodzen@mail.ru>
2024-03-03 21:57:05 +0300

    4.2 kaprekar_check(n)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def numerics(n):
     return list(map(int,str(n)))
 
+def kaprekar_check(n):
+    s=str(n)
+    return len(s) in (3,4,6) and len(set(s))!=1 and n not in [100,1000,100000]
+
 def kaprekar_step(L):
     L.sort()
     return int(''.join(map(str,L[::-1])))-int(''.join(map(str,L)))
