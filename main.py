4716cb4426eb7be36137a7ca16fb5dc18ded8a4f
Jhonny <gonzodzen@mail.ru>
2024-03-03 20:19:33 +0300

    3.2 Derivative

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import os
+from math import exp
+def def_e(x):
+    return round(exp(x),3)
 
-event,file_name='aaa','txt.log'
-x=''
-
-if os.path.isfile(file_name):
-    with open(file_name,'r',encoding='utf-8') as f:
-        x=f.read()
-
-with open(file_name,'w',encoding='utf-8') as f:
-    if x:
-        n=int(x.split()[1])+1
-    else:
-        n=1
-    f.seek(0)
-    f.write(f"event {n} - '{event}'\n"+x)
+print(def_e(int(input())))
