b785535f99c2ef29e34713819bfdd60ca6edb930
Jhonny <gonzodzen@mail.ru>
2024-03-03 14:52:28 +0300

    2.6

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-s=input()
-if s in ['list','str','int']:
-    if s=='int':
-        a,b=int(input()),int(input())
-        if not a and not b:
-            print("Empty Ints")
-        else:
-            print(a+b)
-    elif s=='str':
-        c=input()
-        print(c or "Empty String")
-    else:
-        a=input().split()
-        print(a[-1] if a else "Empty List")
-else:
-    print("Unknown type")
+print(*[i for i in range(int(input())+1)],sep='\n')
