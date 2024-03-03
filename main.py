11daf4622b7cba65788bff764ca8d991e51ab029
Jhonny <gonzodzen@mail.ru>
2024-03-03 18:48:14 +0300

    2.8

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-import os
-
-x=input()
-if os.path.exists(x):
-    if os.path.isfile(x):
-        with open(x,'r',encoding='utf-8') as f:
-            print(f'CONTENT:\n{f.read()}')
-    else:
-        print('ERROR:\nЭто каталог, а не файл')
-else:  
-    print('ERROR:\nФайл не существует')
+with open('output.txt','w',encoding='utf-8') as f:
+    f.write(input())
