76caeb74cf57748c07e0228a417a40ba703863f6
Jhonny <gonzodzen@mail.ru>
2024-03-03 21:44:56 +0300

    4.2 kaprekar_loop(n)

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def numerics(n):
+    return list(map(int,str(n)))
+
 def kaprekar_step(L):
     L.sort()
     return int(''.join(map(str,L[::-1])))-int(''.join(map(str,L)))
 
-print(kaprekar_step([4, 5, 6, 8]))
+def kaprekar_loop(n):
+    print(n)
+    while n!=6174:
+        n=kaprekar_step(numerics(n))
+        print(n)
+
+print(kaprekar_loop(8654))
