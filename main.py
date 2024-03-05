cc195376e42e3777d92d8638a34e579b6bdddeff
Jhonny <gonzodzen@mail.ru>
2024-03-06 00:03:36 +0300

    Indi 4.2

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-n,i,j=int(input()),1,0
-while n>=1:
-    n-=i
-    j+=1
-    i+=j
-print(j-1 or 1)
-
 from bisect import insort
 r,f=[],lambda:[insort(r,int(i)) for i in input().split()]
 input() and f() and f()
