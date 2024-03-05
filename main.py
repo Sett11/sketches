68787ede861e4ed1ca79a8297d0f9d69416350b4
Jhonny <gonzodzen@mail.ru>
2024-03-06 00:17:19 +0300

    Indi 4.2 Ball at BerlSU

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from bisect import insort
-r,f=[],lambda:[insort(r,int(i)) for i in input().split()]
-input() and f() and f()
-print(*r)
+f=lambda:sorted([int(i) for i in input().split()])
+c,a,d,b,t,i=input(),f(),input(),f(),0,0
+while i<len(a):
+    j=0
+    while j in range(len(b)):
+        if abs(a[i]-b[j])<=1:
+            a.pop(i)
+            b.pop(j)
+            t+=1
+            i-=1
+            break
+        j+=1
+    i+=1   
+print(t)
