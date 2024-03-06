d3d22b73359243261336e2542af851a785956591
Jhonny <gonzodzen@mail.ru>
2024-03-07 00:02:28 +0300

    4.4 Kaprekar number in an arbitrary number system

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def multiplication_check_list(start=10, stop=99):
-    a=sum([[bool(i*j) for j in range(start,stop+1)] for i in range(start,stop+1)],[])
-    c=a.count(0)
-    print(f'Правильных результатов: {len(a)-c}',f'Неправильных результатов: {c}',sep='\n')
+from string import digits,ascii_uppercase
 
-multiplication_check_list(96,97)
+def cb(n,b):
+    a,r=digits+ascii_uppercase,''
+    while n:
+        r=a[n%b]+r
+        n//=b
+    return r
+
+        
+def kaprekar(n,base=10):
+    c=int(str(n),base)
+    k=cb(c**2,base)
+    for i in range(1,len(k)):
+        a,b=int(k[:i],base),int(k[i:],base)
+        if a+b==c and (a and b):
+            return True
+    return False
+
+print(kaprekar('F',16))
