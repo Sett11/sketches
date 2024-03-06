a7566c85334368aba900cea9bfffc81a26da4d17
Jhonny <gonzodzen@mail.ru>
2024-03-06 23:16:59 +0300

    4.5 Batch verification of "sage scheme"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-s,r=input(),[]
-while 4<len(s)<10:
-    r.append(s)
-    s=input()
-print(r[-1])
+def multiplication_check_list(start=10, stop=99):
+    a=sum([[bool(i*j) for j in range(start,stop+1)] for i in range(start,stop+1)],[])
+    c=a.count(0)
+    print(f'Правильных результатов: {len(a)-c}',f'Неправильных результатов: {c}',sep='\n')
+
+multiplication_check_list(96,97)
