3197a6c676e49235f9ee03bae60608dc37ee902d
Jhonny <gonzodzen@mail.ru>
2024-03-07 00:44:44 +0300

    indi 4.2 New Year and rush

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n=int(input())
-i=j=k=0
-while i+k<=n:
-    i+=k
+a,b=map(int,input().split())
+i,j,k,q=5,1,0,a
+while b+i<=240 and a:
+    b+=i
     j+=1
-    k=int(input())
-print('Довольно!',i,j-1,sep='\n')
+    i=j*5
+    k+=1
+    a-=1
+print(q if not a else k)
