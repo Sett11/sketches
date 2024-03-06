f15a5ad728a0e5f41a38d040498286d5ef9b2ce8
Jhonny <gonzodzen@mail.ru>
2024-03-06 18:16:11 +0300

    4.2

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-f=lambda:sorted([int(i) for i in input().split()])
-c,a,d,b,t,i=input(),f(),input(),f(),0,0
-while i<len(a):
-    j=0
-    while j in range(len(b)):
-        if abs(a[i]-b[j])<=1:
-            a.pop(i)
-            b.pop(j)
-            t+=1
-            i-=1
-            break
-        j+=1
-    i+=1   
+n,t=int(input()),0
+while n:
+    t+=n
+    n=int(input())
 print(t)
