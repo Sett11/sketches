77fd58d189edf030f7428b44e842a4fd563db797
Jhonny <gonzodzen@mail.ru>
2024-03-07 19:13:15 +0300

    indi 1.8 Electronic watch - 2

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-n=int(input())
-print(n+(1 if n&1 else 2))
+n=int(input())%(1440*3600)
+a=n//3660
+n-=a*3600
+b=n//60
+n-=b*60
+print(*([a]+list(map(lambda s:f'{0 if s<10 else ""}{s}',[b,n]))),sep=':')
