31d531d9eb0e7cccd3ee3418760d0f93b55ae1ba
Jhonny <gonzodzen@mail.ru>
2024-03-07 19:50:25 +0300

    indi 1.10

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-n=int(input())%(1440*3600)
-a=n//3660
-n-=a*3600
-b=n//60
-n-=b*60
-print(*([a]+list(map(lambda s:f'{0 if s<10 else ""}{s}',[b,n]))),sep=':')
+print(all(i%7==0 for i in map(int,input().split())))
