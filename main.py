bbdd20892ed3c4e7e9ddd4d50e0b7b762d0060e0
Jhonny <gonzodzen@mail.ru>
2024-03-07 12:38:23 +0300

    indi 1.5

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-a,b=map(int,input().split())
-i,j,k,q=5,1,0,a
-while b+i<=240 and a:
-    b+=i
-    j+=1
-    i=j*5
-    k+=1
-    a-=1
-print(q if not a else k)
+n,m=float(input()),float(input())
+print(abs(n-m))
