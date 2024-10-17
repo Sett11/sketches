c8c96ddd872eedbbb356740a5d06bbaed19339cb
Jhonny <gonzodzen@mail.ru>
2024-10-18 00:14:13 +0300

    numpy boolean operations

--- numpy_examples/main.py
+++ numpy_examples/main.py
@@ -2,13 +2,36 @@ import numpy as np
 from time import time
 
 
-a=np.arange(1,7)
-b=np.arange(1,4)
-a.resize(2,3)
-print(a*b)
+a=np.arange(1,15)
+b=np.arange(2,16)
+print(a>5)
+print(np.greater(a,b))
+print(a!=8)
+print(np.less(a,b))
+print(a==8)
+print(np.equal(a,b))
+print(np.not_equal(a,b))
+if np.array_equal(a,b):
+    print(9)
+if not np.array_equal(a,b):
+    print(8)
 
-c=np.arange(1,19)
-c.resize(3,3,2)
-d=np.arange(1,7)
-d.resize(3,2)
-print(c*d)
+print(np.any(a>5))
+print(np.all(a>0))
+
+c=np.array([2,3,np.nan,np.inf])
+print(np.isinf(c))
+print(np.isnan(c))
+print(c[~np.isnan(c)])
+print(c[~np.isinf(c)])
+print(c[np.isfinite(c)])
+d=np.array([1.-4.j,2.+5.j-5,3.+0.j])
+print(np.iscomplex(d))
+print(np.isreal(d))
+
+e=np.array([True,False,True,False])
+j=np.array([True]*2+[False]*2)
+print(np.logical_and(e,j))
+print(np.logical_not(e))
+print(np.logical_or(e,j))
+print(np.logical_xor(e,j))
