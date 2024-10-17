c2b0363bacffef7f09bd8ca130427fec20ebcc2b
Jhonny <gonzodzen@mail.ru>
2024-10-18 00:13:20 +0300

    numpy translation arrays

--- numpy_examples/main.py
+++ numpy_examples/main.py
@@ -2,10 +2,13 @@ import numpy as np
 from time import time
 
 
-a=np.arange(1,5)
-b=np.arange(5,9)
-print(a+4)
-print(a*2)
-print(a+b)
-print(b/a)
-print(a**b)
+a=np.arange(1,7)
+b=np.arange(1,4)
+a.resize(2,3)
+print(a*b)
+
+c=np.arange(1,19)
+c.resize(3,3,2)
+d=np.arange(1,7)
+d.resize(3,2)
+print(c*d)
