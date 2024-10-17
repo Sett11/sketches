c7dbcfebb3cf8dd085f1150ab72df97314a1b722
Jhonny <gonzodzen@mail.ru>
2024-10-18 00:46:53 +0300

    numpy matrix and linalg

--- numpy_examples/main.py
+++ numpy_examples/main.py
@@ -2,44 +2,22 @@ import numpy as np
 from time import time
 
 
-a=np.arange(1,11)
-print(np.sum(a))
-print(a.sum())
-print(np.min(a))
-print(a.min())
-print(np.max(a))
-print(a.max())
-print(np.mean(a))
-print(a.mean())
-print(np.median(a))
-print(np.quantile(a,.25))
-print(np.quantile(a,.50))
-print(np.quantile(a,.75))
+a=np.arange(1,10)
+a.resize(3,3)
+b=np.arange(10,19)
+b.resize(3,3)
+print(np.dot(a,b))
+print(np.matmul(a,b))
+print(a@b)
+c,d=np.arange(1,10),np.ones(9)
+print(np.inner(c,d))
+print(np.outer(c,d))
 
-b=np.arange(1,10)
-b.resize(3,2)
-print(b.sum())
-print(b.sum(axis=1))
-print(b.sum(axis=0))
-print(np.amax(a))
-print(np.argmax(a))
-print(np.argmin(a))
-
-print(np.random.rand(5))
-print(np.random.rand(3,3,3))
-print(np.random.randint(5,10,size=(2,2,2)))
-print(np.random.randn(3))
-print(np.random.pareto(3,size=(2,2)))
-print(np.random.beta(3,7,size=(2,2)))
-np.random.seed(13)
-c=np.random.permutation(10)
-print(np.var(c))
-print(np.std(c))
-
-def f(v=True):
-    if v:
-        return sum(range(1,20000000))
-    return np.sum(np.arange(1,20000000))
-
-s=time()
-print(f(False),time()-s)
+x=np.arange(1,4)
+y=x**2
+z=x**3
+k=np.array([x,y,z])
+print(np.linalg.solve(k,np.array([10,20,30])))
+kk=np.linalg.inv(k)
+print(kk)
+print(kk@np.array([10,20,30]))
