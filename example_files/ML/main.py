4f70f7a31e876e658a677e792fb02f9758cae53a
Jhonny <gonzodzen@mail.ru>
2024-11-05 01:57:57 +0300

    Stochastic gradient descent (SGD)

--- example_files/ML/main.py
+++ example_files/ML/main.py
@@ -1,12 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def loss(w,x,y):
+    M=np.dot(w,x)*y
+    return 2/(1+np.exp(M))
+
+def df(w,x,y):
+    M=np.dot(w,x)*y
+    return -2*(1+np.exp(M))**(-2)*np.exp(M)*x*y
+
 x_train = np.array([i+[1] for i in [[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]]])
 y_train = np.array([-1, 1, 1, -1, -1, 1, 1, -1, 1, -1])
 
-pt=np.sum([x*y for x,y in zip(x_train,y_train)],axis=0)
-xxt=np.sum([np.outer(x,x) for x in x_train],axis=0)
-w=np.dot(pt,np.linalg.inv(xxt))
+n_train=len(x_train)
+w=np.zeros(3)
+nt=.0005
+lm=.01
+N=500
+Q=np.mean([loss(w,x,y) for x,y in zip(x_train,y_train)])
+Q_plot=[Q]
+
+for i in range(N):
+    k=np.random.randint(0,n_train-1)
+    ek=loss(w,x_train[k],y_train[k])
+    w=w-nt*df(w,x_train[k],y_train[k])
+    Q=lm*ek+(1-lm)*Q
+    Q_plot.append(Q)
+
+print(w)
+
 
 line_x=list(range(max(x_train[:,0])))
 line_y=[-x*w[0]/w[1]-w[2]/w[1] for x in line_x]
