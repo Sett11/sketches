51788d831ec3ca102d8c0a55a5fcc0d87329ade1
Jhonny <gonzodzen@mail.ru>
2024-11-05 12:27:11 +0300

    Naive Bayes classifier

--- example_files/ML/main.py
+++ example_files/ML/main.py
@@ -1,37 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-def loss(w,x,y):
-    M=np.dot(w,x)*y
-    return 2/(1+np.exp(M))
-
-def df(w,x,y):
-    L1=1.0
-    M=np.dot(w,x)*y
-    return -2*(1+np.exp(M))**(-2)*np.exp(M)*x*y+L1*np.sign(w)
-
-x_train = np.array([x+[10*x[0], 10*x[1], 5*(x[0]+x[1])] for x in [[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]]])
+x_train = np.array([[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]])
 y_train = np.array([-1, 1, 1, -1, -1, 1, 1, -1, 1, -1])
 
-fn=len(x_train[0])
-n_train=len(x_train)
-w=np.zeros(fn)
-nt=.00001
-lm=.01
-N=5000
-Q=np.mean([loss(w,x,y) for x,y in zip(x_train,y_train)])
-Q_plot=[Q]
+mw1, ml1 = np.mean(x_train[y_train == 1], axis=0)
+mw_1, ml_1 = np.mean(x_train[y_train == -1], axis=0)
 
-for i in range(N):
-    k=np.random.randint(0,n_train-1)
-    ek=loss(w,x_train[k],y_train[k])
-    w=w-nt*df(w,x_train[k],y_train[k])
-    Q=lm*ek+(1-lm)*Q
-    Q_plot.append(Q)
+sw1,sl1=np.var(x_train[y_train == 1],axis=0,ddof=1)
+sw_1,sl_1=np.var(x_train[y_train == -1],axis=0,ddof=1)
 
-Q = np.mean([loss(x, w, y) for x, y in zip(x_train, y_train)])
-print(w)
+print('Math expect:', mw1, ml1, mw_1, ml_1)
+print('Dispersion:', sw1, sl1, sw_1, sl_1)
+
+x=[10,40]
+a_1 = lambda x: -np.log(sw_1 * sl_1) - (x[0] - mw_1) ** 2 / (2 * sw_1) - (x[1] - ml_1) ** 2 / (2 * sl_1)
+a1 = lambda x: -np.log(sw1 * sl1) - (x[0] - mw1) ** 2 / (2 * sw1) - (x[1] - ml1) ** 2 / (2 * sl1)
+y = np.argmax([a_1(x), a1(x)]) * 2 - 1
+
+print('Number class (-1 - caterpillar, 1 - ladybug): ',y)
+
+pr=[]
+
+for i in x_train:
+    pr.append(np.argmax([a_1(i),a1(x)])*2-1)
+
+pr=np.array(pr)
+
+Q=np.mean(pr!=y_train)
+
 print(Q)
-plt.plot(Q_plot)
-plt.grid(True)
-plt.show()
