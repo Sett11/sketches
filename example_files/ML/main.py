bea11507a87e51d184b2aeb98525894f39e76585
Jhonny <gonzodzen@mail.ru>
2024-11-05 14:01:37 +0300

    Gaussian Bayes classifier

--- example_files/ML/main.py
+++ example_files/ML/main.py
@@ -1,31 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x_train = np.array([[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]])
-y_train = np.array([-1, 1, 1, -1, -1, 1, 1, -1, 1, -1])
+np.random.seed(1)
 
-mw1, ml1 = np.mean(x_train[y_train == 1], axis=0)
-mw_1, ml_1 = np.mean(x_train[y_train == -1], axis=0)
+r1=.7
+D1=1.0
+mean1=[0,-3]
+V1=[[D1,D1*r1],[D1*r1,D1]]
 
-sw1,sl1=np.var(x_train[y_train == 1],axis=0,ddof=1)
-sw_1,sl_1=np.var(x_train[y_train == -1],axis=0,ddof=1)
+r2=.7
+D2=1.0
+mean2=[0,3]
+V2=[[D2,D2*r2],[D2*r2,D2]]
 
-print('Math expect:', mw1, ml1, mw_1, ml_1)
-print('Dispersion:', sw1, sl1, sw_1, sl_1)
+N=1000
+x1=np.random.multivariate_normal(mean1,V1,N).T
+x2=np.random.multivariate_normal(mean2,V2,N).T
 
-x=[10,40]
-a_1 = lambda x: -np.log(sw_1 * sl_1) - (x[0] - mw_1) ** 2 / (2 * sw_1) - (x[1] - ml_1) ** 2 / (2 * sl_1)
-a1 = lambda x: -np.log(sw1 * sl1) - (x[0] - mw1) ** 2 / (2 * sw1) - (x[1] - ml1) ** 2 / (2 * sl1)
-y = np.argmax([a_1(x), a1(x)]) * 2 - 1
+mm1=np.mean(x1.T,axis=0)
+mm2=np.mean(x2.T,axis=0)
 
-print('Number class (-1 - caterpillar, 1 - ladybug): ',y)
+a=(x1.T-mm1).T
 
-pr=[]
+VV1 = np.array([[np.dot(a[0], a[0]) / N, np.dot(a[0], a[1]) / N],
+                [np.dot(a[1], a[0]) / N, np.dot(a[1], a[1]) / N]])
 
-for i in x_train:
-    pr.append(np.argmax([a_1(i),a1(x)])*2-1)
+a = (x2.T - mm2).T
+VV2 = np.array([[np.dot(a[0], a[0]) / N, np.dot(a[0], a[1]) / N],
+                [np.dot(a[1], a[0]) / N, np.dot(a[1], a[1]) / N]])
 
-pr=np.array(pr)
+Py1,L1=.5,1
+Py2,L2=1-Py1,1
 
-Q=np.mean(pr!=y_train)
+b = lambda x, v, m, l, py: np.log(l * py) - 0.5 * (x - m) @ np.linalg.inv(v) @ (x - m).T - 0.5 * np.log(
+    np.linalg.det(v))
 
-print(Q)
+x=np.array([0,-4])
+
+a=np.argmax([b(x, VV1, mm1, L1, Py1), b(x, VV2, mm2, L2, Py2)])
+
+print(a)
+
+plt.figure(figsize=(7,5))
+plt.title(f'Correlation: r1= {r1}, r2 = {r2}')
+plt.scatter(x1[0],x1[1],s=10)
+plt.scatter(x2[0],x2[1],s=10)
+plt.show()
