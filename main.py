ad70e7402717eed8aec76c1298f5d52170779a73
Jhonny <gonzodzen@mail.ru>
2024-12-04 00:04:04 +0300

    Feigenbaum tree

--- main.py
+++ main.py
@@ -1,114 +1,22 @@
-from math import factorial as f
-from functools import reduce
-from operator import mul
-from collections import Counter
-from itertools import product
-from gmpy2 import is_prime
+import numpy as np
 
-class Universe_item:
-    def __init__(self,number):
-        self.number=number
-        self.history={'previous_numbers':[],'previous_operation':'','this_number':self.number}
+def ferh_eq(n):
+    r,x,a=2.5,.25,[[1]*n for _ in range(n)]
+    f=lambda x:'\n'.join(''.join('-' if j==1 else '+' for j in i) for i in zip(*x))
+    ff=lambda x:sum((sum(i)-n)/len(i) for i in x)/len(x)
+    while r<4:
+        for i in range(1,100):
+            x=r*x*(1-x)
+            if i>50:
+                xs=round(((r-2.5)/1.5)*(n-1))
+                ys=round((1-x)*(n-1))
+                a[xs][ys]+=1
+        r+=.0001
+        d=np.linalg.det(a)
+        if d:
+            print(True,d,r,ff(a))
+            return f(a)
+    print(False,f(a),sep='\n')
+    return r,ff(a)
 
-    def __add__(self,other):
-        new_number=generate_universe_items(self.number+other.number)
-        if isinstance(new_number,str):
-            return new_number
-        new_number.history['previous_numbers']=[self.number,other.number]
-        new_number.history['previous_operation']='+'
-        new_number.history['this_number']=new_number.number
-        return new_number
-    
-    def __sub__(self,other):
-        new_number=generate_universe_items(self.number-other.number)
-        if isinstance(new_number,str):
-            return new_number
-        new_number.history['previous_numbers']=[self.number,other.number]
-        new_number.history['previous_operation']='-'
-        new_number.history['this_number']=new_number.number
-        return new_number
-    
-    def __mul__(self,other):
-        new_number=generate_universe_items(self.number*other.number)
-        if isinstance(new_number,str):
-            return new_number
-        new_number.history['previous_numbers']=[self.number,other.number]
-        new_number.history['previous_operation']='*'
-        new_number.history['this_number']=new_number.number
-        return new_number
-    
-    def __truediv__(self,other):
-        new_number=generate_universe_items(self.number/other.number)
-        if isinstance(new_number,str):
-            return new_number
-        new_number.history['previous_numbers']=[self.number,other.number]
-        new_number.history['previous_operation']='\\'
-        new_number.history['this_number']=new_number.number
-        return new_number
-    
-    def __pow__(self,other):
-        new_number=generate_universe_items(self.number**other.number)
-        if isinstance(new_number,str):
-            return new_number
-        new_number.history['previous_numbers']=[self.number,other.number]
-        new_number.history['previous_operation']='**'
-        new_number.history['this_number']=new_number.number
-        return new_number
-
-    def __str__(self):
-        r=[]
-        for i,j in self.__dict__.items():
-            r.append(f'{i}: {j}')
-        return '\n'.join(r)
-    
-
-def generate_universe_items(n):
-    if n<1 or n>100:
-        return 'This number not exist in my Universe!'
-
-    def pf(n):
-        c,r=2,[]
-        while c<n*n:
-            while n%c==0:
-                r.append(c)
-                n//=c
-            c+=1
-        return r
-    
-    def adn(n):
-        return sorted(map(lambda x:reduce(mul,x),product(*[[j**i for i in range(r+1)] for j,r in Counter(pf(n)).items()])))
-    
-    def fib():
-        a=[1,1]
-        while a[-1]<100:
-            a.append(a[-1]+a[-2])
-        return a
-    
-    def cl(n,r=1):
-        return r if n==1 else cl(n//2,r+1) if n%2==0 else cl(n*3+1,r+1)
-    
-    fib_numbers=fib()
-    UI=Universe_item(n)
-    UI.collatz_length=cl(n)
-    UI.is_even=n%2==0
-    UI.is_odd=n%2!=0
-    UI.factorial=f(n)
-    UI.prime_factors=pf(n)
-    UI.all_divisors=adn(n) if n>1 else [1]
-    UI.is_perfect=n%sum(UI.all_divisors)==0
-    UI.is_fib_number=n in fib_numbers
-    UI.sqrt=n**.5
-    UI.cbrt=pow(n,1/3)
-    UI.sq=n**2
-    UI.cb=n**3
-    UI.is_prime=is_prime(n)
-    return UI
-
-class Universe_numbers:
-    def __init__(self):
-        self.universe_items=[generate_universe_items(i) for i in range(1,101)]
-
-U=Universe_numbers().universe_items
-
-print(U[10]*U[6])
-print(U[70]+U[5])
+print(ferh_eq(111))
