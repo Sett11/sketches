8a81ed47cab6e3203b10ff98d62268f9679509cf
Jhonny <gonzodzen@mail.ru>
2025-01-21 00:03:51 +0300

    Good job save animation

--- project/my_project.py
+++ project/my_project.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from collections import defaultdict
 
-np.random.seed(11)
+np.random.seed(77)
 N=77
 K=1
 L=220
-E=7
+E=5
 R=np.array(range(1,256))
 
 board=np.random.choice(R,size=(N,N))
 all_blobs=set()
 blobs=defaultdict(list)
 
-fig,ax=plt.subplots()
-im=ax.imshow(board,cmap='twilight_shifted_r',interpolation='gaussian')
+fig, ax = plt.subplots(figsize=(10, 10))
+im = ax.imshow(board, cmap='twilight_shifted_r', interpolation='gaussian')
 ax.axis('off')
 
 def one(a):
@@ -32,10 +32,6 @@ def one(a):
                     all_blobs.add((x,y))
                     r[i].append((x,y))
                     a[x,y]=i
-                # else:
-                #     if i>z:
-                #         a[x,y]=i
-                #         r[i].append((x,y))
 
     for i in r:
         blobs[i].extend(r[i])
@@ -49,18 +45,10 @@ def one(a):
                     R=np.fromiter([k for k in R if k!=x],dtype='int32')
                     blobs[a[i,j]].append((i,j))
                     all_blobs.add((i,j))
-                # else:
-                #     for k in blobs:
-                #         if (i,j) in blobs[k] and x>k:
-                #             blobs[k].remove((i,j))
-                #             blobs[x].append((i,j))
-                #             a[i,j]=x
-                #             break
             if (i,j) not in all_blobs:
                 a[i,j]=np.random.choice(R)
                 
     return a
-
 
 def up(k):
     global board
@@ -68,7 +56,8 @@ def up(k):
     im.set_data(board)
     return [im]
 
-anim=FuncAnimation(fig,up,frames=200,interval=200,blit=True)
-plt.show()
+anim = FuncAnimation(fig, up, frames=200, interval=200, blit=True)
 
-anim.save('project_1.gif',writer='imagemagick',fps=10)
+anim.save('animation.gif', writer='pillow', fps=5)
+
+plt.show()
