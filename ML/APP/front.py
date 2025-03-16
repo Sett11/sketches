19c24d13de55454e1c5a79d899301096e2430268
Jhonny <gonzodzen@mail.ru>
2025-03-17 00:00:35 +0300

    New shange for directory

--- ML/APP/front.py
+++ ML/APP/front.py
@@ -6,22 +6,21 @@ def get_resp_model_from_back(input):
     out=requests.get(URL,params={'input':input})
     return out.json()
 
-css = """
-<style>
-    body {
-        background-image: url('image.jpg');
-        background-size: cover;
-        background-position: center;
-    }
-</style>
-"""
-
 with gr.Blocks() as app:
-    gr.Markdown("## ПЕРЕТРИ С ПАЦАНАМИ")
-    
-    input_get_1 = gr.Textbox(label='изрекай здеся', placeholder='братишка, знаешь Гену Бараду...?')
-    output_get = gr.Textbox(label='ответотчка будет тута', placeholder='ща допиздишься, бля...')
-    get_btn=gr.Button('ну чё...?')
-    get_btn.click(fn=get_resp_model_from_back, inputs=input_get_1, outputs=output_get)
+    gr.Markdown("## ФИЛОСОФСКИЕ ДИАЛОГИ",elem_id="title")
+    image_background = gr.Image(value="image_philosophy.png", type="pil", width='100',height='100',interactive=False, visible=True)
+
+    input_get_1 = gr.Textbox(label='Задавайте свой вопрос', placeholder='Ключевые аспекты понятия гармонии в работах Э.М. Сороко')
+    output_get = gr.Textbox(label='Истина где-то рядом...', placeholder='подумаем...')
+    get_btn=gr.Button('попытка - не пытка')
+
+    with gr.Row():
+        with gr.Column(scale=1):
+            get_btn.click(fn=get_resp_model_from_back, inputs=input_get_1, outputs=output_get)
+        with gr.Column(scale=1, min_width=200):
+            input_get_1
+            output_get
+            image_background
+
 
 app.launch(server_name="0.0.0.0", server_port=7860)
