a5d8d15a2b7df315d003fa8dbb18d317b46e8013
Jhonny <gonzodzen@mail.ru>
2025-08-31 00:01:04 +0300

    The unnecessary has been removed.

--- example_files/generate_random_text.py
+++
@@ -1,9 +0,0 @@
-from string import ascii_letters,digits
-from random import randint,sample,shuffle
-
-def generate_random_list():
-    a=list(ascii_letters+digits)
-    shuffle(a)
-    return [''.join(sample(a,randint(1,20))) for _ in range(randint(1,20))]
-
-rand_list=generate_random_list()
